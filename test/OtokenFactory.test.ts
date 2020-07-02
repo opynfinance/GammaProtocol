9aa153463b6cce5f827065097566e27e5129d294
Anton Cheng <antonassocareer@gmail.com>
2020-07-02 17:07:44 +0800

    Ignore no-var-requires rule.

--- test/OtokenFactory.test.ts
+++ test/OtokenFactory.test.ts
@@ -1,13 +1,13 @@
 contract('OtokenFactory Tester', accounts => {
-  const creatorAddress = accounts[0];
+  const creatorAddress = accounts[0]
 
   before('set up contracts', async () => {
     // Do the setup here
-  });
+  })
 
   describe('What are you testing', () => {
     it('Specifics of Test', async () => {
       // Leave this blank for now
-    });
-  });
-});
+    })
+  })
+})
