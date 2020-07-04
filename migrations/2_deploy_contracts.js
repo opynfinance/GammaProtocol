1f469d411a65d9a63c6a5dfa210b212734f884a0
Anton Cheng <antonassocareer@gmail.com>
2020-07-04 17:01:43 +0800

    Add oToken and factory to deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
 const MathTester = artifacts.require('MathTester.sol')
+const OToken = artifacts.require('OToken.sol')
+const OTokenFactory = artifacts.require('OTokenFactory.sol')
 
 module.exports = function(deployer) {
   deployer.then(async () => {
     await deployer.deploy(MathTester)
+    const oTokenLogic = await deployer.deploy(OToken)
+    await deployer.deploy(OTokenFactory, oTokenLogic.address)
   })
 }
