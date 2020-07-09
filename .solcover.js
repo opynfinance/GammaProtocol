951868aacddf520161d5de8b6c659454eaaf96d4
Anton Cheng <antonassocareer@gmail.com>
2020-07-10 00:47:29 +0800

    Exclude oz files from coverage report.

--- .solcover.js
+++ .solcover.js
@@ -4,6 +4,12 @@ module.exports = {
     mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
   },
   skipFiles: [
-    'Migrations.sol'
+    'Migrations.sol',
+    'lib/oz/SafeMath.sol',
+    'lib/oz/Address.sol',
+    'lib/oz/Context.sol',
+    'lib/oz/IERC20.sol',
+    'lib/oz/ERC20Initializable.sol',
+    'lib/oz/Initializable.sol'
   ]
 }
