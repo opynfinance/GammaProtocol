a024299c184a63a2225b8d3508ba6b1db456c3be
Anton Cheng <antonassocareer@gmail.com>
2020-07-10 01:01:11 +0800

    Make the coverage 100%.

--- test/math.test.ts
+++ test/math.test.ts
@@ -17,9 +17,30 @@ contract('Math Tester', () => {
   })
 
   describe('Test Addition', () => {
-    it('Test 2 + 5 = 7', async () => {
+    it('Should return 7 for 5 + 2', async () => {
       const modResult = await math.add('5', '2')
       expect(modResult.toNumber()).to.be.equal(7)
+    })
+  })
+
+  describe('Test subtraction', () => {
+    it('Should return 2 for 7 - 5', async () => {
+      const modResult = await math.sub('7', '5')
+      expect(modResult.toNumber()).to.be.equal(2)
+    })
+  })
+
+  describe('Test Mul', () => {
+    it('Should return 10 for 2 * 5.', async () => {
+      const modResult = await math.mul('2000000000000000000', '5000000000000000000')
+      expect(modResult.toString()).to.be.equal('10000000000000000000')
+    })
+  })
+
+  describe('Test Div', () => {
+    it('Should return 2 for 10 / 5.', async () => {
+      const modResult = await math.div('10000000000000000000', '5000000000000000000')
+      expect(modResult.toString()).to.be.equal('2000000000000000000')
     })
   })
 })
