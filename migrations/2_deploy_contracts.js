925416c8af7c57914301fe6c0ad8d3a32c417ae8
Anton Cheng <antonassocareer@gmail.com>
2020-07-17 01:44:02 +0800

    Remove Otoken from deploy script.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,7 @@
 const MathTester = artifacts.require('MathTester.sol')
-const OToken = artifacts.require('Otoken.sol')
-const OTokenFactory = artifacts.require('OtokenFactory.sol')
 
 module.exports = function(deployer) {
   deployer.then(async () => {
     await deployer.deploy(MathTester)
-    const oTokenLogic = await deployer.deploy(OToken)
-    await deployer.deploy(OTokenFactory, oTokenLogic.address)
   })
 }
