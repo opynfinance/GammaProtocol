b30c674d37bd47ceaf2ad9afb6bef6e16da110db
Haythem Sellami <sellamihaythem96@gmail.com>
2020-07-18 13:25:32 +0100

    project structure

---
+++ contracts/AddressBook.sol
@@ -0,0 +1,12 @@
+/**
+ * SPDX-License-Identifier: UNLICENSED
+ */
+pragma solidity 0.6.10;
+
+/**
+ *
+ */
+// solhint-disable-next-line no-empty-blocks
+contract AddressBook {
+
+}
---
+++ contracts/Controller.sol
@@ -0,0 +1,14 @@
+/**
+ * SPDX-License-Identifier: UNLICENSED
+ */
+pragma solidity 0.6.10;
+
+import {MarginCalculator} from "./MarginCalculator.sol";
+
+/**
+ *
+ */
+// solhint-disable-next-line no-empty-blocks
+contract Controller is MarginCalculator {
+
+}
