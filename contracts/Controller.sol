3a7d9d011b12332675f8a5ea71b03e3be7809f61
Haythem Sellami <sellamihaythem96@gmail.com>
2020-07-18 14:41:20 +0100

    separate controller and margin calculator

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -3,12 +3,10 @@
  */
 pragma solidity 0.6.10;
 
-import {MarginCalculator} from "./MarginCalculator.sol";
-
 /**
  *
  */
 // solhint-disable-next-line no-empty-blocks
-contract Controller is MarginCalculator {
+contract Controller {
 
 }
