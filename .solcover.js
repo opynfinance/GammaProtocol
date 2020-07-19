a889b029795a56bd13b43984d4451c0888cb0a08
Haythem Sellami <sellamihaythem96@gmail.com>
2020-07-19 16:07:04 +0100

    skip OZ files for coverage

--- .solcover.js
+++ .solcover.js
@@ -5,6 +5,11 @@ module.exports = {
   },
   skipFiles: [
     'Migrations.sol',
-    'packages/oz/*.sol',
+    'packages/oz/SafeMath.sol',
+    'packages/oz/Address.sol',
+    'packages/oz/Context.sol',
+    'packages/oz/IERC20.sol',
+    'packages/oz/ERC20Initializable.sol',
+    'packages/oz/Initializable.sol'
   ]
 }
