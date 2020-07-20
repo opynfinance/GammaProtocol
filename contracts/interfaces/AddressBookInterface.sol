01397fddc7aed3c4ee2b7b616d00052fcc03c4bb
Anton Cheng <antonassocareer@gmail.com>
2020-07-20 15:48:49 +0800

    Add AddressBook interface

---
+++ contracts/interfaces/AddressBookInterface.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.10;
+
+interface AddressBookInterface {
+    /* Getters */
+
+    function getOtokenImpl() external view returns (address);
+
+    function getOtokenFactory() external view returns (address);
+
+    function getWhitelist() external view returns (address);
+
+    function getController() external view returns (address);
+
+    function getMarginPool() external view returns (address);
+
+    function getMarginCalculator() external view returns (address);
+
+    function getLiquidationManager() external view returns (address);
+
+    function getAddress(bytes32 id) external view returns (address);
+
+    /* Setters */
+
+    function setOtokenImpl() external;
+
+    function setOtokenFactory() external;
+
+    function setWhitelist() external;
+
+    function setController() external;
+
+    function setMarginPool() external;
+
+    function setMarginCalculator() external;
+
+    function setLiquidationManager() external;
+
+    function setAddress(bytes32 id, address _newImpl) external;
+}
