e6e1ae4ad811d1f60a9a55ce52f2e0ce24befea5
Anton Cheng <antonassocareer@gmail.com>
2020-07-21 12:19:30 +0800

    Update function params

--- contracts/interfaces/WhitelistInterface.sol
+++ contracts/interfaces/WhitelistInterface.sol
@@ -3,12 +3,12 @@ pragma solidity 0.6.10;
 
 interface WhitelistInterface {
     function whitelistProduct(
-        address underlying,
-        address strike,
-        address collateral
+        address _underlying,
+        address _strike,
+        address _collateral
     ) external returns (bytes32 id);
 
-    function whitelistCollateral(address collateral) external returns (bytes32 id);
+    function whitelistCollateral(address _collateral) external returns (bytes32);
 
     function isWhitelistedProduct(
         address underlying,
@@ -16,9 +16,9 @@ interface WhitelistInterface {
         address collateral
     ) external view returns (bool);
 
-    function isWhitelistedCollateral(address collateral) external view returns (bool);
+    function isWhitelistedCollateral(address _collateral) external view returns (bool);
 
-    function registerOtoken(address oToken) external;
+    function registerOtoken(address _oToken) external;
 
-    function isValidOtoken(address oToken) external view returns (bool isValid);
+    function isValidOtoken(address _oToken) external view returns (bool);
 }
