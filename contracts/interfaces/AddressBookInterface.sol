2579d6c45cc251fff8a05e9ebfe22cae072d9bee
Anton Cheng <antonassocareer@gmail.com>
2020-07-22 10:17:12 +0800

    Add AddressBook interface (#17)
    * Add AddressBook interface
    * Update arg names

--- contracts/interfaces/AddressBookInterface.sol
+++ contracts/interfaces/AddressBookInterface.sol
@@ -2,7 +2,39 @@
 pragma solidity 0.6.10;
 
 interface AddressBookInterface {
+    /* Getters */
+
+    function getOtokenImpl() external view returns (address);
+
     function getOtokenFactory() external view returns (address);
 
-    function setOtokenFactory(address _otokenFactory) external;
+    function getWhitelist() external view returns (address);
+
+    function getController() external view returns (address);
+
+    function getMarginPool() external view returns (address);
+
+    function getMarginCalculator() external view returns (address);
+
+    function getLiquidationManager() external view returns (address);
+
+    function getAddress(bytes32 _id) external view returns (address);
+
+    /* Setters */
+
+    function setOtokenImpl(address _otokenImpl) external;
+
+    function setOtokenFactory(address _factory) external;
+
+    function setWhitelist(address _whitelist) external;
+
+    function setController(address _controller) external;
+
+    function setMarginPool(address _marginPool) external;
+
+    function setMarginCalculator(address _calculator) external;
+
+    function setLiquidationManager(address _liquidationManager) external;
+
+    function setAddress(bytes32 _id, address _newImpl) external;
 }
