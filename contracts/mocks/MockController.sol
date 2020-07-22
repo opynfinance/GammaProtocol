8efec0022ba02042ac616307e8e85ac9a13a068f
aparnakr <aparnalocked@gmail.com>
2020-07-22 16:14:28 -0700

    tests for parse deposit actions

---
+++ contracts/mocks/MockController.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma experimental ABIEncoderV2;
+
+import {Actions} from "../libs/Actions.sol";
+
+contract MockController {
+    Actions.DepositArgs private depositArgs;
+
+    function testParseDespositAction(Actions.ActionArgs memory _args) external {
+        depositArgs = Actions._parseDepositArgs(_args);
+    }
+
+    function getDepositArgs() external view returns (Actions.DepositArgs memory) {
+        return depositArgs;
+    }
+}
---
+++ test/Actions.test.ts
@@ -0,0 +1,92 @@
+import {MockControllerInstance} from '../build/types/truffle-types'
+
+const {BN, expectRevert} = require('@openzeppelin/test-helpers')
+
+const MockController = artifacts.require('MockController.sol')
+
+contract('Actions', ([owner, random]) => {
+  // Controller mock instance
+  let controller: MockControllerInstance
+  const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
+
+  before('Deployment', async () => {
+    controller = await MockController.new({from: owner})
+  })
+
+  describe('Parse Deposit Arguments', () => {
+    it('should not be able to parse a non Deposit action', async () => {
+      const actionType = '0'
+      const asset = ZERO_ADDR
+      const vaultId = '0'
+      const amount = '10'
+      const index = '0'
+      const bytesArgs = ZERO_ADDR
+
+      const data = {
+        actionType: actionType,
+        owner: owner,
+        sender: owner,
+        asset: asset,
+        vaultId: vaultId,
+        amount: amount,
+        index: index,
+        data: bytesArgs,
+      }
+
+      await expectRevert(
+        controller.testParseDespositAction(data),
+        'Actions: can only parse arguments for deposit actions',
+      )
+    })
+    it('should not be able to parse an invalid sender address', async () => {
+      const actionType = '3'
+      const asset = ZERO_ADDR
+      const vaultId = '0'
+      const amount = '10'
+      const index = '0'
+      const bytesArgs = ZERO_ADDR
+
+      const data = {
+        actionType: actionType,
+        owner: ZERO_ADDR,
+        sender: owner,
+        asset: asset,
+        vaultId: vaultId,
+        amount: amount,
+        index: index,
+        data: bytesArgs,
+      }
+
+      await expectRevert(controller.testParseDespositAction(data), 'Actions: cannot deposit to an invalid account')
+    })
+    it('should be able to parse arguments for a deposit long action', async () => {
+      const actionType = '3'
+      const asset = ZERO_ADDR
+      const vaultId = '1'
+      const amount = '10'
+      const index = '0'
+      const bytesArgs = ZERO_ADDR
+
+      const data = {
+        actionType: actionType,
+        owner: owner,
+        sender: random,
+        asset: asset,
+        vaultId: vaultId,
+        amount: amount,
+        index: index,
+        data: bytesArgs,
+      }
+
+      await controller.testParseDespositAction(data)
+
+      const depositArgs = await controller.getDepositArgs()
+      assert.equal(depositArgs.owner, owner)
+      assert.equal(depositArgs.amount, new BN(amount))
+      assert.equal(depositArgs.asset, asset)
+      assert.equal(depositArgs.from, random)
+      assert.equal(depositArgs.vaultId, new BN(vaultId))
+      assert.equal(depositArgs.index, new BN(index))
+    })
+  })
+})
