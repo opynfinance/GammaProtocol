6123d048245d8e044e715d8ac6603ad9e1604c40
Zubin Koticha <zkoticha@gmail.com>
2020-07-22 15:09:46 -0700

    clearVault code added

--- contracts/MarginAccount.sol
+++ contracts/MarginAccount.sol
@@ -19,4 +19,17 @@ library MarginAccount {
         //High Level: Increment vault numbers in account
         _account.vaultIds += 1;
     }
+
+    function clearVault(Vault storage vault) internal {
+        //High Level: remove everything in the vault
+        //Reset short, long and collateral assets and amounts arrays to [].
+        //TODO: do we want to ensure enough gas first
+        delete vault.shortAmounts;
+        delete vault.longAmounts;
+        delete vault.collateralAmounts;
+        delete vault.shortOtokens;
+        delete vault.longOtokens;
+        delete vault.collateralAssets;
+        return;
+    }
 }
