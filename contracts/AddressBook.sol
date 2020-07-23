51046c7fba6cf5eb6298c570553ce5118ced1e59
Haythem Sellami <sellamihaythem96@gmail.com>
2020-07-23 17:51:04 +0100

    address book setters+getters+update implementation function

--- contracts/AddressBook.sol
+++ contracts/AddressBook.sol
@@ -3,10 +3,116 @@
  */
 pragma solidity 0.6.10;
 
-/**
- *
- */
-// solhint-disable-next-line no-empty-blocks
-contract AddressBook {
+import {Ownable} from "./packages/oz/Ownable.sol";
+import {OwnedUpgradeabilityProxy} from "./packages/openzeppelin-upgradeability/OwnedUpgradeabilityProxy.sol";
 
+/**
+ * @author Opyn Team
+ * @title AddressBook Module
+ */
+contract AddressBook is Ownable {
+    bytes32 private constant OTOKEN_IMPL = "OTOKEN_IMPL";
+    bytes32 private constant OTOKEN_FACTORY = "OTOKEN_FACTORY";
+    bytes32 private constant MARGIN_WHITELIST = "MARGIN_WHITELIST";
+    bytes32 private constant CONTROLLER = "CONTROLLER";
+    bytes32 private constant MARGIN_POOL = "MARGIN_POOL";
+    bytes32 private constant MARGIN_CALCULATOR = "MARGIN_CALCULATOR";
+    bytes32 private constant LIQUIDATION_MANAGER = "LIQUIDATION_MANAGER";
+    bytes32 private constant ORACLE = "Oracle";
+
+    /// @dev a mapping between module key and module address
+    mapping(bytes32 => address) private modules;
+
+    function getAddress(bytes32 _key) public view returns (address) {
+        return addresses[_key];
+    }
+
+    function getOtokenImpl() external view returns (address) {
+        return getAddress(OTOKEN_IMPL);
+    }
+
+    function getOtokenFactory() external view returns (address) {
+        return getAddress(OTOKEN_FACTORY);
+    }
+
+    function getWhitelist() external view returns (address) {
+        return getAddress(MARGIN_WHITELIST);
+    }
+
+    function getController() external view returns (address) {
+        return getAddress(CONTROLLER);
+    }
+
+    function getMarginPool() external view returns (address) {
+        return getAddress(MARGIN_POOL);
+    }
+
+    function getMarginCalculator() external view returns (address) {
+        return getAddress(MARGIN_CALCULATOR);
+    }
+
+    function getLiquidationManager() external view returns (address) {
+        return getAddress(LIQUIDATION_MANAGER);
+    }
+
+    function getOracle() external view returns (address) {
+        return getAddress(ORACLE);
+    }
+
+    function setAddress(bytes32 _key, address _module) public onlyOwner {
+        modules[_key] = _module;
+    }
+
+    function setOtokenImpl(address _otokenImpl) external onlyOwner {
+        updateImplInternal(OTOKEN_IMPL, _otokenImpl);
+    }
+
+    function setOtokenFactory(address _otokenFactory) external onlyOwner {
+        updateImplInternal(OTOKEN_FACTORY, _otokenFactory);
+    }
+
+    function setWhitelist(address _whitelist) external onlyOwner {
+        updateImplInternal(MARGIN_WHITELIST, _whitelist);
+    }
+
+    function setController(address _controller) external onlyOwner {
+        updateImplInternal(CONTROLLER, _controller);
+    }
+
+    function setMarginPool(address _marginPool) external onlyOwner {
+        updateImplInternal(MARGIN_POOL, _marginPool);
+    }
+
+    function setMarginCalculator(address _marginCalculator) external onlyOwner {
+        updateImplInternal(MARGIN_CALCULATOR, _marginCalculator);
+    }
+
+    function setLiquidationManager(address _liquidationManager) external onlyOwner {
+        updateImplInternal(LIQUIDATION_MANAGER, _liquidationManager);
+    }
+
+    function setOracle(address _oracle) external onlyOwner {
+        updateImplInternal(ORACLE, _oracle);
+    }
+
+    /**
+     * @dev internal function to update the implementation of a specific component of the protocol
+     * @param _id the id of the contract to be updated
+     * @param _newAddress the address of the new implementation
+     **/
+    function updateImplInternal(bytes32 _id, address _newAddress) public onlyOwner {
+        address payable proxyAddress = address(uint160(getAddress(_id)));
+
+        Owned proxy = OwnedUpgradeabilityProxy(proxyAddress);
+        bytes memory params = abi.encodeWithSignature("initialize(address)", address(this));
+
+        if (proxyAddress == address(0)) {
+            proxy = new OwnedUpgradeabilityProxy();
+            proxy.upgradeToAndCall(_newAddress, params);
+            setAddress(_id, address(proxy));
+            emit ProxyCreated(_id, address(proxy));
+        } else {
+            proxy.upgradeToAndCall(_newAddress, params);
+        }
+    }
 }
