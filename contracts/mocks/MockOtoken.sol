8c523b4c46631dc6c4b4b9fce199c940352a7633
Anton Cheng <antonassocareer@gmail.com>
2020-07-25 14:58:29 +0800

    Seperate Functional tests

--- contracts/mocks/MockOtoken.sol
+++ contracts/mocks/MockOtoken.sol
@@ -1,12 +1,12 @@
 pragma solidity =0.6.10;
 
-import {ERC20Initializable} from "../packages/oz/upgradeability/ERC20Initializable.sol";
+// import {ERC20Initializable} from "../packages/oz/upgradeability/ERC20Initializable.sol";
 
 /**
  * SPDX-License-Identifier: UNLICENSED
  * @dev The Otoken inherits ERC20Initializable because we need to use the init instead of constructor.
  */
-contract MockOtoken is ERC20Initializable {
+contract MockOtoken {
     address public underlyingAsset;
     address public strikeAsset;
     address public collateralAsset;
@@ -16,6 +16,8 @@ contract MockOtoken is ERC20Initializable {
 
     bool public isPut;
 
+    bool public inited = false;
+
     function init(
         address _underlyingAsset,
         address _strikeAsset,
@@ -23,15 +25,16 @@ contract MockOtoken is ERC20Initializable {
         uint256 _strikePrice,
         uint256 _expiry,
         bool _isPut
-    ) external initializer {
-        underlyingAsset = _underlyingAsset;
-        strikeAsset = _strikeAsset;
-        collateralAsset = _collateralAsset;
-        strikePrice = _strikePrice;
-        expiry = _expiry;
-        isPut = _isPut;
-        string memory name = "ETHUSDC/1597511955/200P/USDC";
-        string memory symbol = "oETHUSDCP";
-        __ERC20_init_unchained(name, symbol);
+    ) external {
+        inited = true;
+        // underlyingAsset = _underlyingAsset;
+        // strikeAsset = _strikeAsset;
+        // collateralAsset = _collateralAsset;
+        // strikePrice = _strikePrice;
+        // expiry = _expiry;
+        // isPut = _isPut;
+        // string memory name = "ETHUSDC/1597511955/200P/USDC";
+        // string memory symbol = "oETHUSDCP";
+        // __ERC20_init_unchained(name, symbol);
     }
 }
