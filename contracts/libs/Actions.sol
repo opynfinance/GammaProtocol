efb382bf175593054d1613e17690e57bdb05846b
aparnakr <aparnalocked@gmail.com>
2020-07-26 13:28:19 -0700

    tests for mint and burn actions pass

--- contracts/libs/Actions.sol
+++ contracts/libs/Actions.sol
@@ -44,6 +44,42 @@ library Actions {
         bytes data;
     }
 
+    struct MintArgs {
+        /// @notice The address of the account owner
+        address owner;
+        /// @notice The index of the vault from which the asset will be minted
+        uint256 vaultId;
+        /// @notice The address to which we transfer the minted otokens
+        address to;
+        /// @notice The otoken that is to be minted
+        address otoken;
+        /**
+         * @notice Each vault can hold multiple short / long / collateral assets. In this version, we are restricting the scope to only 1 of each.
+         * In future versions this would be the index of the short / long / collateral asset that needs to be modified.
+         */
+        uint256 index;
+        /// @notice The amount of otokens that is to be minted
+        uint256 amount;
+    }
+
+    struct BurnArgs {
+        /// @notice The address of the account owner
+        address owner;
+        /// @notice The index of the vault from which the otoken will be burned
+        uint256 vaultId;
+        /// @notice The address from which we transfer the otokens
+        address from;
+        /// @notice The otoken that is to be burned
+        address otoken;
+        /**
+         * @notice Each vault can hold multiple short / long / collateral assets. In this version, we are restricting the scope to only 1 of each.
+         * In future versions this would be the index of the short / long / collateral asset that needs to be modified.
+         */
+        uint256 index;
+        /// @notice The amount of otokens that is to be burned
+        uint256 amount;
+    }
+
     struct DepositArgs {
         /// @notice The address of the account owner
         address owner;
@@ -60,6 +96,46 @@ library Actions {
         uint256 index;
         /// @notice The amount of asset that is to be transfered
         uint256 amount;
+    }
+
+    /**
+     * @notice Parses the passed in action argmuents to get the argmuents for a mint action
+     * @param _args The general action arguments structure
+     * @return The arguments for a mint action
+     */
+    function _parseMintArgs(ActionArgs memory _args) internal returns (MintArgs memory) {
+        require(_args.actionType == ActionType.MintShortOption, "Actions: can only parse arguments for mint actions");
+        require(_args.owner != address(0), "Actions: cannot mint from an invalid account");
+
+        return
+            MintArgs({
+                owner: _args.owner,
+                vaultId: _args.vaultId,
+                to: _args.sender,
+                otoken: _args.asset,
+                index: _args.index,
+                amount: _args.amount
+            });
+    }
+
+    /**
+     * @notice Parses the passed in action argmuents to get the argmuents for a burn action
+     * @param _args The general action arguments structure
+     * @return The arguments for a burn action
+     */
+    function _parseBurnArgs(ActionArgs memory _args) internal returns (BurnArgs memory) {
+        require(_args.actionType == ActionType.BurnShortOption, "Actions: can only parse arguments for burn actions");
+        require(_args.owner != address(0), "Actions: cannot burn from an invalid account");
+
+        return
+            BurnArgs({
+                owner: _args.owner,
+                vaultId: _args.vaultId,
+                from: _args.sender,
+                otoken: _args.asset,
+                index: _args.index,
+                amount: _args.amount
+            });
     }
 
     /**
--- contracts/mocks/ActionTester.sol
+++ contracts/mocks/ActionTester.sol
@@ -5,12 +5,30 @@ import {Actions} from "../libs/Actions.sol";
 
 contract ActionTester {
     Actions.DepositArgs private depositArgs;
+    Actions.MintArgs private mintArgs;
+    Actions.BurnArgs private burnArgs;
 
-    function testParseDespositAction(Actions.ActionArgs memory _args) external {
+    function testParseDepositAction(Actions.ActionArgs memory _args) external {
         depositArgs = Actions._parseDepositArgs(_args);
     }
 
     function getDepositArgs() external view returns (Actions.DepositArgs memory) {
         return depositArgs;
+    }
+
+    function testParseMintAction(Actions.ActionArgs memory _args) external {
+        mintArgs = Actions._parseMintArgs(_args);
+    }
+
+    function getMintArgs() external view returns (Actions.MintArgs memory) {
+        return mintArgs;
+    }
+
+    function testParseBurnAction(Actions.ActionArgs memory _args) external {
+        burnArgs = Actions._parseBurnArgs(_args);
+    }
+
+    function getBurnArgs() external view returns (Actions.BurnArgs memory) {
+        return burnArgs;
     }
 }
