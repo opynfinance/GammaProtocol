e291e49a9f917c251d31bb460339b439f1e806b5
Zubin Koticha <zkoticha@gmail.com>
2020-07-28 09:54:04 -0700

    MarginAccount test file udpated, MarginAccount added to libs folder

--- contracts/mocks/MarginAccountTester.sol
+++ contracts/mocks/MarginAccountTester.sol
@@ -4,6 +4,9 @@ pragma experimental ABIEncoderV2;
 import {MarginAccount} from "../libs/MarginAccount.sol";
 
 contract ActionTester {
+    using MarginAccount for MarginAccount.Account;
+    using MarginAccount for MarginAccount.Vault;
+
     MarginAccount.Account private account;
     MarginAccount.Vault private vault;
 
@@ -25,5 +28,41 @@ contract ActionTester {
         uint256 _index
     ) external {
         vault._removeShort(_shortOtoken, _amount, _index);
+    }
+
+    function testAddLong(
+        address _longOtoken,
+        uint256 _amount,
+        uint256 _index
+    ) external {
+        vault._addLong(_longOtoken, _amount, _index);
+    }
+
+    function testRemoveLong(
+        address _longOtoken,
+        uint256 _amount,
+        uint256 _index
+    ) external {
+        vault._removeShort(_longOtoken, _amount, _index);
+    }
+
+    function testAddCollateral(
+        address _collateralAsset,
+        uint256 _amount,
+        uint256 _index
+    ) external {
+        vault._addCollateral(_collateralAsset, _amount, _index);
+    }
+
+    function testRemoveCollateral(
+        address _collateralAsset,
+        uint256 _amount,
+        uint256 _index
+    ) external {
+        vault._removeCollateral(_collateralAsset, _amount, _index);
+    }
+
+    function testClearVault() internal {
+        vault._clearVault();
     }
 }
