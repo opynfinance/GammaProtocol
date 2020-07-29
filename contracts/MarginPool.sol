5f34a83241b2e39a4db900d8a86a1c2450fa7c3f
Haythem Sellami <haythem96@users.noreply.github.com>
2020-07-29 19:10:23 +0100

    Pool module: single address transfer (#35)
    * init margin pool: transfer to pool ERC20 assets
    
    * Added in transferToUser function and comments
    
    * transfer in and out assets
    
    * transfer WETH in and out + init unit tests
    
    * remove comment
    
    * fix unit tests
    
    * improve unit tests
    
    * rename functions
    
    * update margin pool
    
    Co-authored-by: aleone <aleone.mit@gmail.com>

--- contracts/MarginPool.sol
+++ contracts/MarginPool.sol
@@ -3,10 +3,90 @@
  */
 pragma solidity 0.6.10;
 
-/**
- *
- */
-// solhint-disable-next-line no-empty-blocks
-contract MarginPool {
+import {ERC20Interface} from "./interfaces/ERC20Interface.sol";
+import {AddressBookInterface} from "./interfaces/AddressBookInterface.sol";
+import {SafeMath} from "./packages/oz/SafeMath.sol";
+import {WETH9} from "./packages/canonical-weth/WETH9.sol";
 
+/**
+ * @author Opyn Team
+ * @title MarginPool
+ * @notice contract that hold all protocol funds
+ */
+contract MarginPool {
+    using SafeMath for uint256;
+
+    /// @notice scaling unit
+    uint256 public constant BASE_UNIT = 1e18;
+
+    /// @notice AddressBook module
+    address public addressBook;
+
+    /**
+     * @notice contructor
+     * @param _addressBook adressbook module
+     */
+    constructor(address _addressBook) public {
+        require(_addressBook != address(0), "Invalid address book");
+
+        addressBook = _addressBook;
+    }
+
+    /**
+     * @notice check if the sender is the Controller module
+     */
+    modifier onlyController() {
+        require(
+            msg.sender == AddressBookInterface(addressBook).getController(),
+            "MarginPool: Sender is not Controller"
+        );
+
+        _;
+    }
+
+    /**
+     * @notice fallback function to reveive ETH
+     */
+    receive() external payable {}
+
+    /**
+     * @notice transfers asset from user to pool
+     * @dev all tokens are scaled to have 1e18 precision in contracts,
+     *      so amounts are scaled down to native token decimals using _calcTransferAmount().
+     *      If _asset equal to WETH address, transfer WETH from Controller address to pool
+     * @param _asset address of asset to transfer
+     * @param _user address of user to transfer assets from
+     * @param _amount amount of token to transfer from _user, scaled to 1e18 of precision
+     * @return true if successful transfer
+     */
+    function transferToPool(
+        address _asset,
+        address _user,
+        uint256 _amount
+    ) external onlyController returns (bool) {
+        require(_amount > 0, "MarginPool: transferToPool amount is below 0");
+
+        // transfer val from _user to pool
+        return ERC20Interface(_asset).transferFrom(_user, address(this), _amount);
+    }
+
+    /**
+     * @notice transfers asset from pool to user
+     * @dev all tokens are scaled to have 1e18 precision in contracts,
+     *      so amounts are scaled down to native token decimals using _calcTransferAmount()
+     * @param _asset address of asset to transfer
+     * @param _user address of user to transfer assets to
+     * @param _amount amount of token to transfer to _user, scaled to 1e18 of precision
+     * @return true if successful transfer
+     */
+    function transferToUser(
+        address _asset,
+        address payable _user,
+        uint256 _amount
+    ) external onlyController returns (bool) {
+        require(_amount > 0, "MarginPool: transferToUser amount is below 0");
+
+        // transfer asset val from Pool to _user
+        return ERC20Interface(_asset).transfer(_user, _amount);
+    }
 }
