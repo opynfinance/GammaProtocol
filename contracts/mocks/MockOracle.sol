1d73c4baf432f133cc33f6f04f9a303271776ac9
Anton Cheng <antonassocareer@gmail.com>
2020-07-30 16:45:29 +0800

    Add MockOracle layout

---
+++ contracts/mocks/MockOracle.sol
@@ -0,0 +1,11 @@
+pragma solidity =0.6.10;
+
+/**
+ * SPDX-License-Identifier: UNLICENSED
+ * @dev The MockOracle contract let us easily manipulate the oracle state in testings.
+ */
+contract MockOracle {
+    function init() external{
+        
+    }
+}
