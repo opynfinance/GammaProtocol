6c37db243b9c8247e71a81126dfd4da761134aef
Haythem Sellami <sellamihaythem96@gmail.com>
2020-07-30 12:34:45 +0100

    NatSpec comment

--- contracts/libs/FixedPointInt256.sol
+++ contracts/libs/FixedPointInt256.sol
@@ -37,26 +37,64 @@ library FixedPointInt256 {
         }
     }
 
+    /**
+     * @notice return the sum of two signed integer
+     * @param a signed integer
+     * @param b signed integer
+     * @return sum of two signed integer
+     */
     function add(int256 a, int256 b) internal pure returns (int256) {
         return a.add(b);
     }
 
+    /**
+     * @notice return the difference of two signed integer
+     * @param a signed integer
+     * @param b signed integer
+     * @return difference of two signed integer
+     */
     function sub(int256 a, int256 b) internal pure returns (int256) {
         return a.sub(b);
     }
 
+    /**
+     * @notice multiply two signed integer
+     * @dev rounds to zero if a*b < SCALING_FACTOR / 2
+     * @param a signed integer
+     * @param b signed integer
+     * @return mul of two signed integer
+     */
     function mul(int256 a, int256 b) internal pure returns (int256) {
         return add(mul(a, b), SCALING_FACTOR / 2) / SCALING_FACTOR;
     }
 
+    /**
+     * @notice divide two signed integer
+     * @dev rounds to zero if a*b < SCALING_FACTOR / 2
+     * @param a signed integer
+     * @param b signed integer
+     * @return div of two signed integer
+     */
     function div(int256 a, int256 b) internal pure returns (int256) {
         return add(mul(a, SCALING_FACTOR), b / 2) / b;
     }
 
+    /**
+     * @notice the minimum between a and b
+     * @param a signed integer
+     * @param b signed integer
+     * @return min of two signed integer
+     */
     function min(int256 a, int256 b) internal pure returns (int256) {
         return a < b ? a : b;
     }
 
+    /**
+     * @notice the maximum between a and b
+     * @param a signed integer
+     * @param b signed integer
+     * @return max of two signed integer
+     */
     function max(int256 a, int256 b) internal pure returns (int256) {
         return a > b ? a : b;
     }
