58838565c257427b0afb7891a547e49e13ae26a5
Haythem Sellami <sellamihaythem96@gmail.com>
2020-07-30 20:59:17 +0100

    init oracle contract

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -4,6 +4,7 @@
 pragma solidity 0.6.10;
 
 import "./interfaces/AggregatorInterface.sol";
+import "./interfaces/AddressBookInterface.sol";
 import "./packages/oz/Ownable.sol";
 
 /**
@@ -12,5 +13,31 @@ import "./packages/oz/Ownable.sol";
  * @notice The Oracle module provide the system with on-chain prices
  */
 contract Oracle is Ownable {
+    /// @dev structure that represent price, and timestamp
+    struct Price {
+        uint256 price;
+        uint256 timestamp; // timestamp at which the price is pulled to this oracle
+    }
 
+    /// @notice AddressBook module
+    address public addressBook;
+
+    /// @dev mapping between oracle and it's locking period
+    mapping(address => uint256) internal oracleLockingPeriod;
+    /// @dev mapping between oracle and it's dispute period
+    mapping(address => uint256) internal oracleDisputePeriod;
+    /// @dev mapping between batch and it's oracle
+    mapping(bytes32 => address) internal batchOracle;
+    /// @dev mapping between batch and it price at specific timestmap
+    mapping(bytes32 => mapping(uint256 => Price)) internal batchPriceAt;
+
+    /**
+     * @dev constructor
+     * @param _addressBook AddressBook module address
+     */
+    constructor(address _addressBook) public {
+        require(_addressBook != address(0), "Invalid address book");
+
+        addressBook = _addressBook;
+    }
 }
