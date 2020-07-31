c53ea3e5632265be869ef1e38a35b10f10780976
Haythem Sellami <haythem96@users.noreply.github.com>
2020-07-31 23:47:39 +0100

    Oracle module basis (#53)
    * init oracle
    
    * init oracle contract
    
    * init tests
    
    * Update contracts/Oracle.sol
    
    Co-authored-by: Anton Cheng <antonassocareer@gmail.com>
    
    * Update contracts/Oracle.sol
    
    Co-authored-by: aparnakr <aparnalocked@gmail.com>
    
    * Update contracts/Oracle.sol
    
    Co-authored-by: aparnakr <aparnalocked@gmail.com>
    
    * remove addressbook
    
    Co-authored-by: Anton Cheng <antonassocareer@gmail.com>
    Co-authored-by: aparnakr <aparnalocked@gmail.com>

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -3,10 +3,28 @@
  */
 pragma solidity 0.6.10;
 
-/**
- *
- */
-// solhint-disable-next-line no-empty-blocks
-contract Oracle {
+import "./interfaces/AggregatorInterface.sol";
+import "./interfaces/AddressBookInterface.sol";
+import "./packages/oz/Ownable.sol";
 
+/**
+ * @author Opyn Team
+ * @title Oracle Module
+ * @notice The Oracle module provide the system with on-chain prices
+ */
+contract Oracle is Ownable {
+    /// @dev structure that represent price, and timestamp
+    struct Price {
+        uint256 price;
+        uint256 timestamp; // timestamp at which the price is pulled to this oracle
+    }
+
+    /// @dev mapping between oracle and it's locking period
+    mapping(address => uint256) internal oracleLockingPeriod;
+    /// @dev mapping of asset price to it's dispute period
+    mapping(address => uint256) internal oracleDisputePeriod;
+    /// @dev mapping between batch and it's oracle
+    mapping(bytes32 => address) internal batchOracle;
+    /// @dev mapping between batch and it price at specific timestmap
+    mapping(bytes32 => mapping(uint256 => Price)) internal batchPriceAt;
 }
