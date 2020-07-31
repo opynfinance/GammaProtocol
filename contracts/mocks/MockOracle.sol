aadaccc942ad5a0376f9ad6b58a32c6884ee865b
Anton Cheng <antonassocareer@gmail.com>
2020-07-31 10:07:24 +0800

    Mock oracle

--- contracts/mocks/MockOracle.sol
+++ contracts/mocks/MockOracle.sol
@@ -5,7 +5,20 @@ pragma solidity =0.6.10;
  * @dev The MockOracle contract let us easily manipulate the oracle state in testings.
  */
 contract MockOracle {
-    function init() external{
-        
+    address public addressBook;
+    uint256 public mockedPrice;
+    bool public mockedFinalized;
+
+    constructor(address _addressBook) public {
+        addressBook = _addressBook;
+    }
+
+    function getPrice(bytes32 _btach, uint256 _timestamp) external view returns (uint256, bool) {
+        return (mockedPrice, mockedFinalized);
+    }
+
+    function setMockedStatus(uint256 _pirce, bool _finalized) external {
+        mockedPrice = _pirce;
+        mockedFinalized = _finalized;
     }
 }
