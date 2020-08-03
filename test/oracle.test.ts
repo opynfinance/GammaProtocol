80de6af81cdcda3252973fa680183bef2f70909f
Haythem Sellami <haythem96@users.noreply.github.com>
2020-08-03 14:42:40 +0100

    Update test/oracle.test.ts
    Co-authored-by: Anton Cheng <antonassocareer@gmail.com>

--- test/oracle.test.ts
+++ test/oracle.test.ts
@@ -146,7 +146,7 @@ contract('Oracle', ([owner, controllerAddress, random]) => {
       await batchOracle.setRoundAnswer(roundBack, batchUnderlyingPrice)
 
       // current timestamp at which price will be submitted to our Oracle module
-      const onchainPriceTimestmap = new BigNumber(await time.latest())
+      const onchainPriceTimestamp = new BigNumber(await time.latest())
       // set batch underlying price
       await oracle.setBatchUnderlyingPrice(batch, batchExpiry, roundBack, {from: controllerAddress})
 
