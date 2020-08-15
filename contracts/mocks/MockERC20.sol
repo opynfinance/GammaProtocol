218059ecf7687302fac8e70e195152f4c820c51b
Anton Cheng <antonassocareer@gmail.com>
2020-08-15 18:16:04 +0800

    Fix Syntic report: Local variables shadowing - (10)

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -4,8 +4,8 @@ pragma solidity 0.6.10;
 import {ERC20Initializable} from "../packages/oz/upgradeability/ERC20Initializable.sol";
 
 contract MockERC20 is ERC20Initializable {
-    constructor(string memory name, string memory symbol) public {
-        __ERC20_init_unchained(name, symbol);
+    constructor(string memory _name, string memory _symbol) public {
+        __ERC20_init_unchained(_name, _symbol);
     }
 
     function mint(address account, uint256 amount) public {
