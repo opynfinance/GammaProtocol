63d603069131bc37dc5016d31dcf2f2f68bb3525
Anton Cheng <antonassocareer@gmail.com>
2020-08-17 21:51:40 +0800

    Removoe MathTester (#63)
    * Removoe MathTester
    
    * Remove MathTester deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,3 @@
-const MathTester = artifacts.require('MathTester.sol')
-
 module.exports = function(deployer) {
-  deployer.then(async () => {
-    await deployer.deploy(MathTester)
-  })
+  deployer.then(async () => {})
 }
