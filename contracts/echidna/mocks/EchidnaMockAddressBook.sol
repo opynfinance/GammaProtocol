47818fdd6ad1d2a7916e83d128047b3ea109f9ee
aparnakr <aparnalocked@gmail.com>
2020-08-19 21:01:51 -0700

    unit tests and echidna tests for otoken pass

--- contracts/echidna/mocks/EchidnaMockAddressBook.sol
+++ contracts/echidna/mocks/EchidnaMockAddressBook.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.10;
 
-contract MockAddressBook {
+contract EchidnaMockAddressBook {
     constructor() public {}
 
     function getController() external view returns (address) {
