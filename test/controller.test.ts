428981263b24395c5a5cc98724e1ab90cf979ba4
Haythem Sellami <sellamihaythem96@gmail.com>
2020-08-25 15:14:27 +0100

    add simple tests

--- test/controller.test.ts
+++ test/controller.test.ts
@@ -1,23 +1,45 @@
-import {MockAddressBookInstance, ControllerInstance} from '../build/types/truffle-types'
+import {
+  MockChainlinkOracleInstance,
+  MockAddressBookInstance,
+  OracleInstance,
+  ControllerInstance,
+} from '../build/types/truffle-types'
+import BigNumber from 'bignumber.js'
 
 const {expectRevert} = require('@openzeppelin/test-helpers')
 
+const MockChainlinkOracle = artifacts.require('MockChainlinkOracle.sol')
 const MockAddressBook = artifacts.require('MockAddressBook.sol')
+const Oracle = artifacts.require('Oracle.sol')
 const Controller = artifacts.require('Controller.sol')
 
 // address(0)
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
 
-contract('Controller', ([owner1, operator1, random]) => {
+contract('Controller', ([owner, accountOwner1, accountOperator1, random]) => {
+  const batch = web3.utils.asciiToHex('ETHUSDCUSDC1596218762')
+  // Chainlink mock instance
+  let batchOracle: MockChainlinkOracleInstance
+  // Oracle module
+  let oracle: OracleInstance
   // addressbook module mock
   let addressBook: MockAddressBookInstance
   // controller module
   let controller: ControllerInstance
 
   before('Deployment', async () => {
+    // addressbook
     addressBook = await MockAddressBook.new()
+    // deploy price feed mock
+    batchOracle = await MockChainlinkOracle.new({from: owner})
+    // deploy Oracle module
+    oracle = await Oracle.new(addressBook.address, {from: owner})
+    // set oracle in AddressBook
+    await addressBook.setOracle(oracle.address)
     // deploy Controller module
     controller = await Controller.new(addressBook.address)
+    // set controller address in AddressBook
+    await addressBook.setController(controller.address, {from: owner})
 
     assert.equal(await controller.systemPaused(), false, 'System is paused')
   })
@@ -30,9 +52,23 @@ contract('Controller', ([owner1, operator1, random]) => {
 
   describe('Account operator', () => {
     it('should set operator', async () => {
-      await controller.setOperator(operator1, true, {from: owner1})
+      await controller.setOperator(accountOperator1, true, {from: accountOwner1})
 
-      assert.equal(await controller.isOperator(owner1, operator1), true, 'Operator address mismatch')
+      assert.equal(await controller.isOperator(accountOwner1, accountOperator1), true, 'Operator address mismatch')
+    })
+  })
+
+  describe('Vault', () => {
+    // will be improved in later PR
+    it('should get vault', async () => {
+      const vaultId = new BigNumber(0)
+      await controller.getVault(accountOwner1, vaultId)
+    })
+
+    // will be improved in later PR
+    it('should get vault balance', async () => {
+      const vaultId = new BigNumber(0)
+      await controller.getVaultBalances(accountOwner1, vaultId)
     })
   })
 
