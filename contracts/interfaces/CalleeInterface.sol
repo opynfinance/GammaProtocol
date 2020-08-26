5b64de521bff4c55935217367397e458aa7403c5
Anton Cheng <antonassocareer@gmail.com>
2020-08-26 19:45:30 +0800

    Remove blank line

--- contracts/interfaces/CalleeInterface.sol
+++ contracts/interfaces/CalleeInterface.sol
@@ -9,7 +9,6 @@ pragma solidity 0.6.10;
 interface CalleeInterface {
     /**
      * Allows users to send this contract arbitrary data.
-     *
      * @param sender The msg.sender to Controller
      * @param owner The vault owner
      * @param data Arbitrary data given by the sender
