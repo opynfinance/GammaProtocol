865dbb538be2e188e6582b324fed2b672a791d74
Haythem Sellami <sellamihaythem96@gmail.com>
2020-09-03 15:15:19 +0100

    unit test

--- test/controller.test.ts
+++ test/controller.test.ts
@@ -1502,6 +1502,80 @@ contract('Controller', ([owner, accountOwner1, accountOperator1, random]) => {
     })
   })
 
+  describe('Short otoken', () => {
+    let longOtoken: MockOtokenInstance
+    let shortOtoken: MockOtokenInstance
+
+    before(async () => {
+      const expiryTime = new BigNumber(60 * 60 * 24) // after 1 day
+
+      longOtoken = await MockOtoken.new()
+      shortOtoken = await MockOtoken.new()
+      // init otoken
+      await longOtoken.init(
+        addressBook.address,
+        weth.address,
+        usdc.address,
+        usdc.address,
+        new BigNumber(250).times(new BigNumber(10).exponentiatedBy(18)),
+        new BigNumber(await time.latest()).plus(expiryTime),
+        true,
+      )
+      await shortOtoken.init(
+        addressBook.address,
+        weth.address,
+        usdc.address,
+        usdc.address,
+        new BigNumber(200).times(new BigNumber(10).exponentiatedBy(18)),
+        new BigNumber(await time.latest()).plus(expiryTime),
+        true,
+      )
+
+      // give free money
+      await longOtoken.mint(accountOwner1, new BigNumber('100'))
+      await longOtoken.mint(accountOperator1, new BigNumber('100'))
+      await usdc.mint(accountOwner1, new BigNumber('1000000'))
+      await usdc.mint(accountOperator1, new BigNumber('1000000'))
+    })
+
+    describe('Mint short otoken', () => {
+      it('mint naked short otoken', async () => {
+        const vaultCounter = new BigNumber(await controller.getAccountVaultCounter(accountOwner1))
+        assert.isAbove(vaultCounter.toNumber(), 0, 'Account owner have no vault')
+
+        console.log(await controller.getVault(accountOwner1, vaultCounter.toNumber()))
+
+        const collateralToDeposit = new BigNumber(await shortOtoken.strikePrice()).dividedBy(1e18)
+        const amountToMint = new BigNumber('1')
+        const actionArgs = [
+          {
+            actionType: ActionType.MintShortOption,
+            owner: accountOwner1,
+            sender: accountOwner1,
+            asset: shortOtoken.address,
+            vaultId: vaultCounter.toNumber(),
+            amount: amountToMint.toNumber(),
+            index: '0',
+            data: ZERO_ADDR,
+          },
+          {
+            actionType: ActionType.DepositCollateral,
+            owner: accountOwner1,
+            sender: accountOwner1,
+            asset: usdc.address,
+            vaultId: vaultCounter.toNumber(),
+            amount: collateralToDeposit.toNumber(),
+            index: '0',
+            data: ZERO_ADDR,
+          },
+        ]
+
+        await usdc.approve(marginPool.address, collateralToDeposit)
+        await controller.operate(actionArgs, {from: accountOwner1})
+      })
+    })
+  })
+
   describe('Check if price is finalized', () => {
     let expiredOtoken: MockOtokenInstance
     let expiry: BigNumber
