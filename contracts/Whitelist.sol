849531536e0668afbad4dfb9dfbbbf4445846667
Haythem Sellami <sellamihaythem96@gmail.com>
2020-09-03 01:32:16 +0100

    add blacklist functions

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -36,10 +36,21 @@ contract Whitelist is Ownable {
         address indexed strike,
         address indexed collateral
     );
+    /// @notice emitted when owner blacklist a product
+    event ProductBlacklisted(
+        bytes32 productHash,
+        address indexed underlying,
+        address indexed strike,
+        address indexed collateral
+    );
     /// @notice emits an event when a collateral address is whitelisted by the owner address
     event CollateralWhitelisted(address indexed collateral);
+    /// @notice emits an event when a collateral address is blacklist by the owner address
+    event CollateralBlacklisted(address indexed collateral);
     /// @notice emitted when Otoken Factory module whitelist an otoken
-    event OtokenWhitelisted(address otoken);
+    event OtokenWhitelisted(address indexed otoken);
+    /// @notice emitted when owner blacklist an otoken
+    event OtokenBlacklisted(address indexed otoken);
 
     /**
      * @notice check if the sender is the Otoken Factory module
@@ -105,9 +116,29 @@ contract Whitelist is Ownable {
     ) external onlyOwner {
         bytes32 productHash = keccak256(abi.encode(_underlying, _strike, _collateral));
 
-        _setWhitelistedProduct(productHash);
+        _setWhitelistedProduct(productHash, true);
 
         emit ProductWhitelisted(productHash, _underlying, _strike, _collateral);
+    }
+
+    /**
+     * @notice allow owner to blacklist product
+     * @dev a product is the hash of the underlying, collateral and strike assets
+     * can only be called from owner address
+     * @param _underlying asset that the option references
+     * @param _strike asset that the strike price is denominated in
+     * @param _collateral asset that is held as collateral against short/written options
+     */
+    function blacklistProduct(
+        address _underlying,
+        address _strike,
+        address _collateral
+    ) external onlyOwner {
+        bytes32 productHash = keccak256(abi.encode(_underlying, _strike, _collateral));
+
+        _setWhitelistedProduct(productHash, false);
+
+        emit ProductBlacklisted(productHash, _underlying, _strike, _collateral);
     }
 
     /**
@@ -116,49 +147,68 @@ contract Whitelist is Ownable {
      * @param _collateral collateral asset address
      */
     function whitelistCollateral(address _collateral) external onlyOwner {
-        _setWhitelistedCollateral(_collateral);
+        _setWhitelistedCollateral(_collateral, true);
 
         emit CollateralWhitelisted(_collateral);
     }
 
     /**
+     * @notice whitelist a collateral address, can only be called by owner
+     * @dev function can only be called by owner
+     * @param _collateral collateral asset address
+     */
+    function blacklistCollateral(address _collateral) external onlyOwner {
+        _setWhitelistedCollateral(_collateral, false);
+
+        emit CollateralBlacklisted(_collateral);
+    }
+
+    /**
      * @notice allow Otoken Factory to whitelist a new option
-     * @dev can only be called from the owner's address
+     * @dev can only be called from the Otoken Factory address
      * @param _otokenAddress otoken
      */
     function whitelistOtoken(address _otokenAddress) external onlyFactory {
-        _setWhitelistedOtoken(_otokenAddress);
+        _setWhitelistedOtoken(_otokenAddress, true);
 
         emit OtokenWhitelisted(_otokenAddress);
     }
 
     /**
-     * @notice set a product hash as whitelisted
-     * @param _productHash product hash in bytes
+     * @notice allow owner to blacklist a new option
+     * @dev can only be called from the owner's address
+     * @param _otokenAddress otoken
      */
-    function _setWhitelistedProduct(bytes32 _productHash) internal {
-        require(!whitelistedProduct[_productHash], "Whitelist: Product already whitelisted");
+    function blacklistOtoken(address _otokenAddress) external onlyOwner {
+        _setWhitelistedOtoken(_otokenAddress, false);
 
-        whitelistedProduct[_productHash] = true;
+        emit OtokenBlacklisted(_otokenAddress);
+    }
+
+    /**
+     * @notice set product state
+     * @param _productHash product hash in bytes
+     * @param _state represent if a product is whitelisted or not, true to whitelist and false to blacklist
+     */
+    function _setWhitelistedProduct(bytes32 _product, bool _state) internal {
+        whitelistedProduct[_productHash] = _state;
     }
 
     /**
      * @notice set a collateral address as whitelisted
      * @param _collateral collateral address
+     * @param _state represent if a collateral is whitelisted or not, true to whitelist and false to blacklist
      */
-    function _setWhitelistedCollateral(address _collateral) internal {
-        require(!whitelistedCollateral[_collateral], "Whitelist: Collateral already whitelisted");
-
-        whitelistedCollateral[_collateral] = true;
+    function _setWhitelistedCollateral(address _collateral, bool _state) internal {
+        whitelistedCollateral[_collateral] = _state;
     }
 
     /**
      * @notice set an otoken address as whitelisted
      * @param _otokenAddress address of the oToken that is being whitelisted
+     * @param _state represent if a Otoken is whitelisted or not, true to whitelist and false to blacklist
      */
-    function _setWhitelistedOtoken(address _otokenAddress) internal {
-        require(!whitelistedOtoken[_otokenAddress], "Whitelist: Otoken already whitelisted");
-
-        whitelistedOtoken[_otokenAddress] = true;
+    function _setWhitelistedOtoken(address _otokenAddress, bool _state) internal {
+        whitelistedOtoken[_otokenAddress] = _state;
     }
 }
