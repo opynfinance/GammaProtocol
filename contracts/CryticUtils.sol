3ac6204edaa82fee4954a054aa33f599a675dbd8
Anton Cheng <antonassocareer@gmail.com>
2020-09-12 23:02:41 +0800

    Test config

--- contracts/CryticUtils.sol
+++ contracts/CryticUtils.sol
@@ -5,4 +5,5 @@ contract CryticUtils {
     address internal crytic_owner = address(0x627306090abaB3A6e1400e9345bC60c78a8BEf57);
     address internal crytic_user = address(0xf17f52151EbEF6C7334FAD080c5704D77216b732);
     address internal crytic_attacker = address(0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef);
+    address internal _fake_controller = address(0xA163e3e565d67C71778060F9dc907Cf92D13CE4A);
 }
---
+++ contracts/otokenErc20.sol
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: UNLICENSED */
+/* solhint-disable */
+pragma solidity =0.6.10;
+
+import "./CryticUtils.sol";
+// import "./EchidnaAddressBook.sol";
+import "../Otoken.sol";
+import "../mocks/MockAddressBook.sol";
+import "../mocks/MockERC20.sol";
+import "../interfaces/AddressBookInterface.sol";
+
+contract OtokenERC20 is CryticUtils, Otoken {
+    constructor() public {
+        // Existing addresses:
+        // - crytic_owner: If the contract has an owner, it must be crytic_owner
+        // - crytic_user: Legitimate user
+        // - crytic_attacker: Attacker
+        _initializeForFuzzer();
+    }
+
+    /**
+     * Can't use the init function on otoken because it's external
+     */
+    function _initializeForFuzzer() internal initializer {
+        MockAddressBook _addressBook = new MockAddressBook();
+        _addressBook.setController(_fake_controller);
+
+        address usdc = address(new MockERC20("USDC", "USDC", 6));
+        address weth = address(new MockERC20("WETH", "WETH", 18));
+
+        addressBook = address(_addressBook);
+        underlyingAsset = usdc;
+        strikeAsset = weth;
+        collateralAsset = weth;
+        strikePrice = 200 * 1e18;
+        expiry = now + 100 days;
+        isPut = true;
+        (string memory tokenName, string memory tokenSymbol) = _getNameAndSymbol();
+        __ERC20_init_unchained(tokenName, tokenSymbol);
+    }
+
+    function echidna_always_empty() public view returns (bool) {
+        return this.balanceOf(address(0x0)) == 0;
+    }
+
+    function echidna_less_than_total() public view returns (bool) {
+        assert(this.balanceOf(crytic_owner) <= totalSupply());
+        assert(this.balanceOf(crytic_user) <= totalSupply());
+        assert(this.balanceOf(crytic_attacker) <= totalSupply());
+        return true;
+    }
+}
