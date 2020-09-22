946e57e9973404c7ba6d85f1d5a6c512e43a5739
aparnakr <aparnalocked@gmail.com>
2020-09-21 19:44:11 -0700

    add naked call tests

---
+++ test/integration-tests/nakedCallExpireOtm.test.ts
@@ -0,0 +1,299 @@
+import {
+  MockERC20Instance,
+  MarginCalculatorInstance,
+  AddressBookInstance,
+  MockOracleInstance,
+  OtokenInstance,
+  ControllerInstance,
+  WhitelistInstance,
+  MarginPoolInstance,
+  OtokenFactoryInstance,
+} from '../../build/types/truffle-types'
+import {createTokenAmount, createValidExpiry} from '../utils'
+import BigNumber from 'bignumber.js'
+
+const {time} = require('@openzeppelin/test-helpers')
+const AddressBook = artifacts.require('AddressBook.sol')
+const MockOracle = artifacts.require('MockOracle.sol')
+const Otoken = artifacts.require('Otoken.sol')
+const MockERC20 = artifacts.require('MockERC20.sol')
+const MarginCalculator = artifacts.require('MarginCalculator.sol')
+const Whitelist = artifacts.require('Whitelist.sol')
+const MarginPool = artifacts.require('MarginPool.sol')
+const Controller = artifacts.require('Controller.sol')
+const MarginAccount = artifacts.require('MarginAccount.sol')
+const OTokenFactory = artifacts.require('OtokenFactory.sol')
+const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
+
+enum ActionType {
+  OpenVault,
+  MintShortOption,
+  BurnShortOption,
+  DepositLongOption,
+  WithdrawLongOption,
+  DepositCollateral,
+  WithdrawCollateral,
+  SettleVault,
+  Exercise,
+  Call,
+}
+
+contract('Naked Call Option expires Otm flow', ([accountOwner1, buyer]) => {
+  let expiry: number
+
+  let addressBook: AddressBookInstance
+  let calculator: MarginCalculatorInstance
+  let controllerProxy: ControllerInstance
+  let controllerImplementation: ControllerInstance
+  let marginPool: MarginPoolInstance
+  let whitelist: WhitelistInstance
+  let otokenImplementation: OtokenInstance
+  let otokenFactory: OtokenFactoryInstance
+
+  // oracle modulce mock
+  let oracle: MockOracleInstance
+
+  let usdc: MockERC20Instance
+  let weth: MockERC20Instance
+
+  let ethCall: OtokenInstance
+  const strikePrice = 300
+
+  const optionsAmount = 10
+  const collateralAmount = optionsAmount
+  let vaultCounter: number
+
+  const usdcDecimals = 6
+  const wethDecimals = 18
+
+  before('set up contracts', async () => {
+    const now = (await time.latest()).toNumber()
+    expiry = createValidExpiry(now, 30)
+
+    // setup usdc and weth
+    usdc = await MockERC20.new('USDC', 'USDC', usdcDecimals)
+    weth = await MockERC20.new('WETH', 'WETH', wethDecimals)
+
+    // initiate addressbook first.
+    addressBook = await AddressBook.new()
+    // setup calculator
+    calculator = await MarginCalculator.new(addressBook.address)
+    // setup margin pool
+    marginPool = await MarginPool.new(addressBook.address)
+    // setup margin account
+    const lib = await MarginAccount.new()
+    // setup controllerProxy module
+    await Controller.link('MarginAccount', lib.address)
+    controllerImplementation = await Controller.new(addressBook.address)
+    // setup mock Oracle module
+    oracle = await MockOracle.new(addressBook.address)
+    // setup whitelist module
+    whitelist = await Whitelist.new(addressBook.address)
+    await whitelist.whitelistCollateral(weth.address)
+    whitelist.whitelistProduct(weth.address, usdc.address, usdc.address, true)
+    whitelist.whitelistProduct(weth.address, usdc.address, weth.address, false)
+    // setup otoken
+    otokenImplementation = await Otoken.new()
+    // setup factory
+    otokenFactory = await OTokenFactory.new(addressBook.address)
+
+    // setup address book
+    await addressBook.setOracle(oracle.address)
+    await addressBook.setMarginCalculator(calculator.address)
+    await addressBook.setWhitelist(whitelist.address)
+    await addressBook.setMarginPool(marginPool.address)
+    await addressBook.setOtokenFactory(otokenFactory.address)
+    await addressBook.setOtokenImpl(otokenImplementation.address)
+    await addressBook.setController(controllerImplementation.address)
+
+    const controllerProxyAddress = await addressBook.getController()
+    controllerProxy = await Controller.at(controllerProxyAddress)
+
+    await otokenFactory.createOtoken(
+      weth.address,
+      usdc.address,
+      weth.address,
+      createTokenAmount(strikePrice, 18),
+      expiry,
+      false,
+    )
+
+    const ethCallAddress = await otokenFactory.getOtoken(
+      weth.address,
+      usdc.address,
+      weth.address,
+      createTokenAmount(strikePrice, 18),
+      expiry,
+      false,
+    )
+
+    ethCall = await Otoken.at(ethCallAddress)
+    // mint weth to user
+    const account1OwnerWeth = createTokenAmount(2 * collateralAmount, wethDecimals)
+    weth.mint(accountOwner1, account1OwnerWeth)
+
+    // have the user approve all the weth transfers
+    weth.approve(marginPool.address, account1OwnerWeth, {from: accountOwner1})
+
+    const vaultCounterBefore = new BigNumber(await controllerProxy.getAccountVaultCounter(accountOwner1))
+    vaultCounter = vaultCounterBefore.toNumber() + 1
+  })
+
+  describe('Integration test: Sell a naked put and close it after expires OTM', () => {
+    const scaledOptionsAmount = createTokenAmount(optionsAmount, 18)
+    const scaledCollateralAmount = createTokenAmount(collateralAmount, wethDecimals)
+    const expirySpotPrice = 200
+    it('Seller should be able to open a short call option', async () => {
+      const actionArgs = [
+        {
+          actionType: ActionType.OpenVault,
+          owner: accountOwner1,
+          sender: accountOwner1,
+          asset: ZERO_ADDR,
+          vaultId: vaultCounter,
+          amount: '0',
+          index: '0',
+          data: ZERO_ADDR,
+        },
+        {
+          actionType: ActionType.MintShortOption,
+          owner: accountOwner1,
+          sender: accountOwner1,
+          asset: ethCall.address,
+          vaultId: vaultCounter,
+          amount: scaledOptionsAmount,
+          index: '0',
+          data: ZERO_ADDR,
+        },
+        {
+          actionType: ActionType.DepositCollateral,
+          owner: accountOwner1,
+          sender: accountOwner1,
+          asset: weth.address,
+          vaultId: vaultCounter,
+          amount: scaledCollateralAmount,
+          index: '0',
+          data: ZERO_ADDR,
+        },
+      ]
+
+      await controllerProxy.operate(actionArgs, {from: accountOwner1})
+    })
+
+    it('Seller: close an OTM position after expiry', async () => {
+      // Keep track of balances before
+      const ownerWethBalanceBefore = new BigNumber(await weth.balanceOf(accountOwner1))
+      const marginPoolWethBalanceBefore = new BigNumber(await weth.balanceOf(marginPool.address))
+      const ownerOtokenBalanceBefore = new BigNumber(await ethCall.balanceOf(accountOwner1))
+      const oTokenSupplyBefore = new BigNumber(await ethCall.totalSupply())
+
+      // Check that we start at a valid state
+      const vaultBefore = await controllerProxy.getVault(accountOwner1, vaultCounter)
+      const vaultStateBefore = await calculator.getExcessCollateral(vaultBefore)
+      assert.equal(vaultStateBefore[0].toString(), '0')
+      assert.equal(vaultStateBefore[1], true)
+
+      // Set the oracle price
+      if ((await time.latest()) < expiry) {
+        await time.increaseTo(expiry + 2)
+      }
+      const scaledETHPrice = createTokenAmount(expirySpotPrice, 18)
+      const scaledUSDCPrice = createTokenAmount(1, 18)
+      await oracle.setExpiryPriceFinalizedAllPeiodOver(weth.address, expiry, scaledETHPrice, true)
+      await oracle.setExpiryPriceFinalizedAllPeiodOver(usdc.address, expiry, scaledUSDCPrice, true)
+
+      // Check that after expiry, the vault excess balance has updated as expected
+      const vaultStateBeforeSettlement = await calculator.getExcessCollateral(vaultBefore)
+      assert.equal(vaultStateBeforeSettlement[0].toString(), scaledCollateralAmount)
+      assert.equal(vaultStateBeforeSettlement[1], true)
+
+      const actionArgs = [
+        {
+          actionType: ActionType.SettleVault,
+          owner: accountOwner1,
+          sender: accountOwner1,
+          asset: ZERO_ADDR,
+          vaultId: vaultCounter,
+          amount: '0',
+          index: '0',
+          data: ZERO_ADDR,
+        },
+      ]
+
+      await controllerProxy.operate(actionArgs, {from: accountOwner1})
+
+      // keep track of balances after
+      const ownerWethBalanceAfter = new BigNumber(await weth.balanceOf(accountOwner1))
+      const marginPoolWethBalanceAfter = new BigNumber(await weth.balanceOf(marginPool.address))
+
+      const ownerOtokenBalanceAfter = new BigNumber(await ethCall.balanceOf(accountOwner1))
+      const oTokenSupplyAfter = new BigNumber(await ethCall.totalSupply())
+
+      // check balances before and after changed as expected
+      assert.equal(ownerWethBalanceBefore.plus(scaledCollateralAmount).toString(), ownerWethBalanceAfter.toString())
+      assert.equal(
+        marginPoolWethBalanceBefore.minus(scaledCollateralAmount).toString(),
+        marginPoolWethBalanceAfter.toString(),
+      )
+      assert.equal(ownerOtokenBalanceBefore.toString(), ownerOtokenBalanceAfter.toString())
+      assert.equal(oTokenSupplyBefore.toString(), oTokenSupplyAfter.toString())
+
+      // Check that we end at a valid state
+      const vaultAfter = await controllerProxy.getVault(accountOwner1, vaultCounter)
+      const vaultStateAfter = await calculator.getExcessCollateral(vaultAfter)
+      assert.equal(vaultStateAfter[0].toString(), '0')
+      assert.equal(vaultStateAfter[1], true)
+
+      // Check the vault balances stored in the contract
+      assert.equal(vaultAfter.shortOtokens.length, 0, 'Length of the short otoken array in the vault is incorrect')
+      assert.equal(vaultAfter.collateralAssets.length, 0, 'Length of the collateral array in the vault is incorrect')
+      assert.equal(vaultAfter.longOtokens.length, 0, 'Length of the long otoken array in the vault is incorrect')
+
+      assert.equal(vaultAfter.shortAmounts.length, 0, 'Length of the short amounts array in the vault is incorrect')
+      assert.equal(
+        vaultAfter.collateralAmounts.length,
+        0,
+        'Length of the collateral amounts array in the vault is incorrect',
+      )
+      assert.equal(vaultAfter.longAmounts.length, 0, 'Length of the long amounts array in the vault is incorrect')
+    })
+
+    it('Buyer: exercise OTM call option after expiry', async () => {
+      // owner sells their call option
+      ethCall.transfer(buyer, scaledOptionsAmount, {from: accountOwner1})
+
+      // Keep track of balances before
+      const ownerWethBalanceBefore = new BigNumber(await weth.balanceOf(buyer))
+      const marginPoolWethBalanceBefore = new BigNumber(await weth.balanceOf(marginPool.address))
+      const ownerOtokenBalanceBefore = new BigNumber(await ethCall.balanceOf(buyer))
+      const oTokenSupplyBefore = new BigNumber(await ethCall.totalSupply())
+
+      const actionArgs = [
+        {
+          actionType: ActionType.Exercise,
+          owner: buyer,
+          sender: buyer,
+          asset: ethCall.address,
+          vaultId: '0',
+          amount: scaledOptionsAmount,
+          index: '0',
+          data: ZERO_ADDR,
+        },
+      ]
+
+      await controllerProxy.operate(actionArgs, {from: buyer})
+
+      // keep track of balances after
+      const ownerWethBalanceAfter = new BigNumber(await weth.balanceOf(buyer))
+      const marginPoolWethBalanceAfter = new BigNumber(await weth.balanceOf(marginPool.address))
+      const ownerOtokenBalanceAfter = new BigNumber(await ethCall.balanceOf(buyer))
+      const oTokenSupplyAfter = new BigNumber(await ethCall.totalSupply())
+
+      // check balances before and after changed as expected
+      assert.equal(ownerWethBalanceBefore.toString(), ownerWethBalanceAfter.toString())
+      assert.equal(marginPoolWethBalanceBefore.toString(), marginPoolWethBalanceAfter.toString())
+      assert.equal(ownerOtokenBalanceBefore.minus(scaledOptionsAmount).toString(), ownerOtokenBalanceAfter.toString())
+      assert.equal(oTokenSupplyBefore.minus(scaledOptionsAmount).toString(), oTokenSupplyAfter.toString())
+    })
+  })
+})
