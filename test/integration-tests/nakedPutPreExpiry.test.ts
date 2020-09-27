ae0824c487cdb07c54847ca3a01dc3412a481d14
Anton Cheng <antonassocareer@gmail.com>
2020-09-27 17:38:01 +0800

    Remove expectRevert.unspecified

--- test/integration-tests/nakedPutPreExpiry.test.ts
+++ test/integration-tests/nakedPutPreExpiry.test.ts
@@ -409,8 +409,10 @@ contract('Naked Put Option closed before expiry flow', ([accountOwner1, buyer]) 
           data: ZERO_ADDR,
         },
       ]
-      // TODO: Revert message to be updated
-      await expectRevert.unspecified(controllerProxy.operate(actionArgs, {from: accountOwner1}))
+      await expectRevert(
+        controllerProxy.operate(actionArgs, {from: accountOwner1}),
+        'Controller: invalid final vault state',
+      )
     })
 
     it('should be able to transfer long otokens to another address', async () => {
