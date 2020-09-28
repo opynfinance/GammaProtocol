ddf2603348747d0971e42b0c900be10134d3dde5
aleone <aleone.mit@gmail.com>
2020-09-28 09:49:28 -0400

    Otoken comment edits

--- contracts/Otoken.sol
+++ contracts/Otoken.sol
@@ -10,13 +10,13 @@ import {AddressBookInterface} from "./interfaces/AddressBookInterface.sol";
 /**
  * @title Otoken
  * @author Opyn Team
- * @notice Otoken is the ERC20 token for an option.
- * @dev The Otoken inherits ERC20Initializable because we need to use the init instead of constructor.
+ * @notice Otoken is the ERC20 token for an option
+ * @dev The Otoken inherits ERC20Initializable because we need to use the init instead of constructor
  */
 contract Otoken is ERC20Initializable {
     using SafeMath for uint256;
 
-    /// @notice address of the addressBook module
+    /// @notice address of the AddressBook module
     address public addressBook;
 
     /// @notice asset that the option references
@@ -31,22 +31,22 @@ contract Otoken is ERC20Initializable {
     /// @notice strike price with decimals = 18
     uint256 public strikePrice;
 
-    /// @notice time of the option represented by unix timestamp
+    /// @notice expiration timestamp of the option, represented as a unix timestamp
     uint256 public expiryTimestamp;
 
-    /// @notice is this a put option, if not it is a call
+    /// @notice True if a put option, False if a call option
     bool public isPut;
 
     uint256 private constant STRIKE_PRICE_DIGITS = 1e18;
 
     /**
-     * @notice initialize the otoken.
+     * @notice initialize the oToken
      * @param _underlyingAsset asset that the option references
      * @param _strikeAsset asset that the strike price is denominated in
      * @param _collateralAsset asset that is held as collateral against short/written options
      * @param _strikePrice strike price with decimals = 18
-     * @param _expiryTimestamp time of the option represented by unix timestamp
-     * @param _isPut is this a put option, if not it is a call
+     * @param _expiryTimestamp expiration timestamp of the option, represented as a unix timestamp
+     * @param _isPut True if a put option, False if a call option
      */
     function init(
         address _addressBook,
@@ -69,45 +69,47 @@ contract Otoken is ERC20Initializable {
     }
 
     /**
-     * @notice Mint oToken for an account.
-     * @dev this is a Controller only method. Access control is taken care of by _beforeTokenTransfer hook.
-     * @param account the account to mint token to
-     * @param amount the amount to mint
+     * @notice mint oToken for an account
+     * @dev Controller only method where access control is taken care of by _beforeTokenTransfer hook
+     * @param account account to mint token to
+     * @param amount amount to mint
      */
     function mintOtoken(address account, uint256 amount) external {
         _mint(account, amount);
     }
 
     /**
-     * @notice Burn oToken from an account.
-     * @dev this is a Controller only method. Access control is taken care of by _beforeTokenTransfer hook.
-     * @param account the account to burn token from
-     * @param amount the amount to burn
+     * @notice burn oToken from an account.
+     * @dev Controller only method where access control is taken care of by _beforeTokenTransfer hook
+     * @param account account to burn token from
+     * @param amount amount to burn
      */
     function burnOtoken(address account, uint256 amount) external {
         _burn(account, amount);
     }
 
     /**
-     * @notice generate name and symbol for an option
-     * @dev this function use named return variable to avoid stack-too-deep error
-     * @return tokenName ETHUSDC 05-September-2020 200 Put USDC Collateral
-     * @return tokenSymbol oETHUSDC-05SEP20-200P
+     * @notice generates the name and symbol for an option
+     * @dev this function uses a named return variable to avoid the stack-too-deep error
+     * @return tokenName (ex: ETHUSDC 05-September-2020 200 Put USDC Collateral)
+     * @return tokenSymbol (ex: oETHUSDC-05SEP20-200P)
      */
     function _getNameAndSymbol() internal view returns (string memory tokenName, string memory tokenSymbol) {
         string memory underlying = _getTokenSymbol(underlyingAsset);
         string memory strike = _getTokenSymbol(strikeAsset);
         string memory collateral = _getTokenSymbol(collateralAsset);
         uint256 displayedStrikePrice = strikePrice.div(STRIKE_PRICE_DIGITS);
-        // convert expiry to readable string
+
+        // convert expiry to a readable string
         (uint256 year, uint256 month, uint256 day) = BokkyPooBahsDateTimeLibrary.timestampToDate(expiryTimestamp);
 
-        // Get option type string
+        // get option type string
         (string memory typeSymbol, string memory typeFull) = _getOptionType(isPut);
 
+        //get option month string
         (string memory monthSymbol, string memory monthFull) = _getMonth(month);
 
-        // concat name string: ETHUSDC 05-September-2020 200 Put USDC Collateral
+        // concatenated name string: ETHUSDC 05-September-2020 200 Put USDC Collateral
         tokenName = string(
             abi.encodePacked(
                 underlying,
@@ -127,7 +129,7 @@ contract Otoken is ERC20Initializable {
             )
         );
 
-        // concat symbol string: oETHUSDC-05SEP20-200P
+        // concatenated symbol string: oETHUSDC-05SEP20-200P
         tokenSymbol = string(
             abi.encodePacked(
                 "o",
@@ -154,8 +156,8 @@ contract Otoken is ERC20Initializable {
     }
 
     /**
-     * @dev Internal function to get the number with 2 characters.
-     * @return The 2 characters for the number.
+     * @dev return a representation of a number using 2 characters, adds a leading 0 if one digit, uses two trailing digits if a 3 digit number
+     * @return 2 characters that corresponds to a number
      */
     function _uintTo2Chars(uint256 number) internal pure returns (string memory) {
         string memory str = Strings.toString(number);
@@ -168,9 +170,9 @@ contract Otoken is ERC20Initializable {
     }
 
     /**
-     * @dev return string of option type
-     * @return shortString P or C
-     * @return longString Put or Call
+     * @dev return string representation of option type
+     * @return shortString a 1 character representation of option type (P or C)
+     * @return longString a full length string of option type (Put or Call)
      */
     function _getOptionType(bool _isPut) internal pure returns (string memory shortString, string memory longString) {
         if (_isPut) {
@@ -181,9 +183,9 @@ contract Otoken is ERC20Initializable {
     }
 
     /**
-     * @dev return string of month.
-     * @return shortString SEP, DEC ...etc
-     * @return longString September, December ...etc
+     * @dev return string representation of a month
+     * @return shortString a 3 character representation of a month (ex: SEP, DEC, etc)
+     * @return longString a full length string of a month (ex: September, December, etc)
      */
     function _getMonth(uint256 _month) internal pure returns (string memory shortString, string memory longString) {
         if (_month == 1) {
@@ -214,9 +216,9 @@ contract Otoken is ERC20Initializable {
     }
 
     /**
-     * @dev this function overrides the _beforeTokenTransfer hook in ERC20Initializable.sol.
-     * If the operation is mint or burn, requires msg.sender to be the controller.
-     * The function signature is the same as _beforeTokenTransfer defined in ERC20Initializable.sol.
+     * @dev this function overrides the _beforeTokenTransfer hook in ERC20Initializable.sol
+     * if the operation is mint or burn, requires msg.sender to be the controller
+     * the function signature is the same as _beforeTokenTransfer defined in ERC20Initializable.sol
      * @param from from address
      * @param to to address
      * @param amount amount to transfer
