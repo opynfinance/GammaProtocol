cd699b3cc0f43de48d7a0df72b791975ea34a372
Andrew <aleone.mit@gmail.com>
2020-10-03 09:18:06 -0400

    Misc naming / comments changes (#255)

--- contracts/libs/SignedConverter.sol
+++ contracts/libs/SignedConverter.sol
@@ -4,13 +4,15 @@
 pragma solidity 0.6.10;
 
 /**
- *
+ * @title SignedConverter
+ * @author Opyn Team
+ * @notice A library to convert an unsigned integer to signed integer or signed integer to unsigned integer.
  */
 library SignedConverter {
     /**
-     * @notice convert an unsigned integer to signed integer
-     * @param a uint to convert into a signed integer.
-     * @return the converted signed integer.
+     * @notice convert an unsigned integer to a signed integer
+     * @param a uint to convert into a signed integer
+     * @return converted signed integer
      */
     function uintToInt(uint256 a) internal pure returns (int256) {
         require(a < 2**255, "FixedPointInt256: out of int range");
@@ -19,9 +21,9 @@ library SignedConverter {
     }
 
     /**
-     * @notice convert a signed integer to unsigned integer
-     * @param a int to convert into an unsigned integer.
-     * @return the converted unsigned integer.
+     * @notice convert a signed integer to an unsigned integer
+     * @param a int to convert into an unsigned integer
+     * @return converted unsigned integer
      */
     function intToUint(int256 a) internal pure returns (uint256) {
         if (a < 0) {
