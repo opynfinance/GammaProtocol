0caf07714fa10070d91b8e5f67e1c4509508f719
Shelly Grossman <shellygr151@gmail.com>
2020-10-08 21:27:06 +0300

    run from the package

--- specs/runMarginVault.sh
+++ specs/runMarginVault.sh
@@ -1,1 +1,1 @@
-certoraRun.py MarginVaultHarness.sol --solc solc6.10 --verify MarginVaultHarness:MarginVault.spec --settings  -assumeUnwindCond,-b=3,-enableStorageVariableUnpacking=false,-useNonLinearArithmetic --path .. --staging
+certoraRun MarginVaultHarness.sol --solc solc6.10 --verify MarginVaultHarness:MarginVault.spec --settings  -assumeUnwindCond,-b=3,-enableStorageVariableUnpacking=false,-useNonLinearArithmetic --path .. --staging
