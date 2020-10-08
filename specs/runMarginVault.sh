5cc0a0c1df191df0358187cbc8ebce1f73677226
Shelly Grossman <shellygr151@gmail.com>
2020-10-08 15:11:34 +0300

    First specs are in

---
+++ specs/runMarginVault.sh
@@ -0,0 +1,1 @@
+certoraRun.py MarginVaultHarness.sol --solc solc6.10 --verify MarginVaultHarness:MarginVault.spec --settings  -assumeUnwindCond,-b=3,-enableStorageVariableUnpacking=false,-useNonLinearArithmetic --path .. --staging
---
+++ specs/runPrivilegedWhitelist.sh
@@ -0,0 +1,1 @@
+certoraRun.py ../contracts/Whitelist.sol --verify Whitelist:Privileged.spec --solc solc6.10 --settings -b=4 --path .. --staging
