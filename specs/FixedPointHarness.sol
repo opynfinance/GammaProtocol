f0cbf113ee8d1597d9c7b619be831a72722e9967
mshrieve <mshrieve@gmail.com>
2020-10-14 19:32:34 -0700

    expanding the spec

--- specs/FixedPointHarness.sol
+++ specs/FixedPointHarness.sol
@@ -14,9 +14,17 @@ import '../contracts/libs/FixedPointInt256.sol';
 contract FixedPointHarness {
   using FixedPointInt256 for FixedPointInt256.FixedPointInt;
 
+  function testFPI(uint256 _a) external pure returns (uint256) {
+    FixedPointInt256.FixedPointInt memory a = FixedPointInt256.fromScaledUint(_a, 18);
+    return FixedPointInt256.toScaledUint(a, 18, true);
+  }
+
   function testAdd(uint256 _a, uint256 _b) external pure returns (uint256) {
-    FixedPointInt256.FixedPointInt memory a = FixedPointInt256.fromUnscaledInt(int256(_a));
-    FixedPointInt256.FixedPointInt memory b = FixedPointInt256.fromUnscaledInt(int256(_b));
-    return FixedPointInt256.toScaledUint(a.add(b), 18, true);
+    require(_b == 0);
+    FixedPointInt256.FixedPointInt memory a = FixedPointInt256.fromScaledUint(_a, 18);
+    FixedPointInt256.FixedPointInt memory b = FixedPointInt256.fromScaledUint(_b, 18);
+
+    FixedPointInt256.FixedPointInt memory c = FixedPointInt256.add(a, b);
+    return FixedPointInt256.toScaledUint(c, 18, true);
   }
 }
--- specs/fixedPoint.spec
+++ specs/fixedPoint.spec
@@ -1,13 +1,27 @@
 methods {
     testAdd (uint256, uint256) returns uint256 envfree
+    testFPI (uint256) returns uint256 envfree
 }
 
+rule testFPI(uint256 a)
+description "test fpi" 
+{
+    uint256 c = sinvoke testFPI(a);
+    assert a == c, "failed conversion test";
+}
 
+rule testAdditionByZero(uint256 a)
+description "test addition" 
+{
+    uint256 c = sinvoke testAdd(a, 0);
+
+    assert c == a, "failed addition by zero test";
+}
 
 rule testAddition(uint256 a, uint256 b)
 description "test addition" 
 {
-    uint256 c = a + b;
-    uint256 cScaled = sinvoke testAdd(a, b);
-    assert c * (10^18) == cScaled, "failed addition test";
+    uint256 c = sinvoke testAdd(a, b);
+
+    assert a + b == c, "failed addition test";
 }
