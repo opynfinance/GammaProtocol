f51e7a005146440d4809177a6fd35e61840bfbc7
aparnakr <aparnalocked@gmail.com>
2020-10-14 18:15:17 -0700

    change harness

--- specs/.certora_config/FixedPointHarness.sol_0/4_FixedPointHarness.sol
+++ specs/.certora_config/FixedPointHarness.sol_0/4_FixedPointHarness.sol
@@ -15,8 +15,8 @@ contract FixedPointHarness {
   using FixedPointInt256 for FixedPointInt256.FixedPointInt;
 
   function testAdd(uint256 _a, uint256 _b) external pure returns (uint256) {
-    FixedPointInt256.FixedPointInt memory a = FixedPointInt256.fromUnscaledInt(int256(_a));
-    FixedPointInt256.FixedPointInt memory b = FixedPointInt256.fromUnscaledInt(int256(_b));
-    return FixedPointInt256.toScaledUint(a.add(b), 18, true);
+    FixedPointInt256.FixedPointInt memory a = FixedPointInt256.fromScaledUint(_a, 0);
+    FixedPointInt256.FixedPointInt memory b = FixedPointInt256.fromScaledUint(_b, 0);
+    return FixedPointInt256.toScaledUint(a.add(b), 0, true);
   }
 }
--- specs/fixedPoint.spec
+++ specs/fixedPoint.spec
@@ -9,5 +9,5 @@ description "test addition"
 {
     uint256 c = a + b;
     uint256 cScaled = sinvoke testAdd(a, b);
-    assert c * (10^18) == cScaled, "failed addition test";
+    assert c == cScaled, "failed addition test";
 }
