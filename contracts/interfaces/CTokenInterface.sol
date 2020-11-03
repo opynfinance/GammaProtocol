4d348e4a96fcc17f5f6ea0bb3a502ef33b66b443
Anton Cheng <antonassocareer@gmail.com>
2020-11-03 23:48:56 +0800

    Add ERC20 interface for CTokens

--- contracts/interfaces/CTokenInterface.sol
+++ contracts/interfaces/CTokenInterface.sol
@@ -3,15 +3,22 @@
  */
 pragma solidity 0.6.10;
 
+import {ERC20Interface} from "./ERC20Interface.sol";
+
 /**
  * @dev Interface of Compound cToken
  */
-interface CTokenInterface {
+interface CTokenInterface is ERC20Interface {
     /**
      * @notice Calculates the exchange rate from the underlying to the CToken
      * @return Calculated exchange rate scaled by 1e18
      */
     function exchangeRateStored() external view returns (uint256);
 
-    function decimals() external view returns (uint256);
+    /**
+     * msg.sender: The account which shall supply the asset, and own the minted cTokens.
+     * @param _mintAmount: The amount of the asset to be supplied, in units of the underlying asset.
+     * RETURN: 0 on success, otherwise an Error code
+     */
+    function mint(uint256 _mintAmount) external returns (uint256);
 }
