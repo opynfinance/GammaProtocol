e4f6ebfc6f12ab81f3d7cb622566aa183fc239d5
Haythem Sellami <haythem96@users.noreply.github.com>
2020-11-19 23:13:18 +0100

    [L01] Disputer can dispute a price before a pricer has submitted a price (#292)
    * fix L01
    
    * add tests
    
    * remove timestamp update

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -221,6 +221,9 @@ contract Oracle is Ownable {
         require(!isDisputePeriodOver(_asset, _expiryTimestamp), "Oracle: dispute period over");
 
         Price storage priceToUpdate = storedPrice[_asset][_expiryTimestamp];
+
+        require(priceToUpdate.timestamp != 0, "Oracle: price to dispute does not exist");
+
         uint256 oldPrice = priceToUpdate.price;
         priceToUpdate.price = _price;
 
