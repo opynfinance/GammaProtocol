f6ecb6b53f56d5dc29e74c2314726db001be37e2
Haythem Sellami <sellamihaythem96@gmail.com>
2020-11-30 12:36:35 +0100

    update CI scripts

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 CI=true truffle test ./test/e2e/*.ts
--- ci/ganache_fork.sh
+++ ci/ganache_fork.sh
@@ -1,1 +1,3 @@
-ganache-cli --fork https://mainnet.infura.io/v3/$INFURA_KEY --unlock 0xbe0eb53f46cd790cd13851d5eff43d12404d33e8
+#!/bin/sh
+
+$(npm bin)/ganache-cli --fork https://mainnet.infura.io/v3/$INFURA_KEY --unlock 0xbe0eb53f46cd790cd13851d5eff43d12404d33e8
