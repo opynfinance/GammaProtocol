a416ce61fdc4380cd0de85d81f09715e04dcb4a7
Nurit Dor <nurit@certora.com>
2020-12-08 10:10:12 +0200

    issue with the new local type checking.
    will enable back after fix

--- specs/scripts/runMarginVault.sh
+++ specs/scripts/runMarginVault.sh
@@ -1,1 +1,1 @@
-certoraRun specs/harness/MarginVaultHarness.sol --solc solc --verify MarginVaultHarness:specs/MarginVault.spec --settings  -assumeUnwindCond,-b=3,-enableStorageVariableUnpacking=false,-useNonLinearArithmetic 
+certoraRun specs/harness/MarginVaultHarness.sol --solc solc --verify MarginVaultHarness:specs/MarginVault.spec --settings  -assumeUnwindCond,-b=3,-enableStorageVariableUnpacking=false,-useNonLinearArithmetic --disableLocalTypeChecking 
