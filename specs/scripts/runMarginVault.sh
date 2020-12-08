040e40a6afb7ae76aa39716f582d8629567ee7f9
Nurit Dor <nurit@certora.com>
2020-12-08 14:24:07 +0200

    move to Certora production

--- specs/scripts/runMarginVault.sh
+++ specs/scripts/runMarginVault.sh
@@ -1,1 +1,1 @@
-certoraRun specs/harness/MarginVaultHarness.sol --solc solc --verify MarginVaultHarness:specs/MarginVault.spec --settings  -assumeUnwindCond,-b=3,-enableStorageVariableUnpacking=false,-useNonLinearArithmetic --disableLocalTypeChecking 
+certoraRun specs/harness/MarginVaultHarness.sol --solc solc --verify MarginVaultHarness:specs/MarginVault.spec --settings  -assumeUnwindCond,-b=3,-enableStorageVariableUnpacking=false,-useNonLinearArithmetic  
