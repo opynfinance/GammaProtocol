d948a88365fbf3c3d9ad1c5cad15275022d3c900
Shelly Grossman <shellygr151@gmail.com>
2021-01-01 18:00:19 +0200

    fix

--- specs/scripts/runMarginVault.sh
+++ specs/scripts/runMarginVault.sh
@@ -1,1 +1,1 @@
-certoraRun specs/harness/MarginVaultHarness.sol --solc solc --verify MarginVaultHarness:specs/MarginVault.spec --settings  -assumeUnwindCond,-b=3,-ciMode=true,-useNonLinearArithmetic  
+certoraRun specs/harness/MarginVaultHarness.sol --solc solc --verify MarginVaultHarness:specs/MarginVault.spec --settings  -assumeUnwindCond,-b=3,-ciMode=true,-useNonLinearArithmetic  --staging shelly/opynFullIntegration
