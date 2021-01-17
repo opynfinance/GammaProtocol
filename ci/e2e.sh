ab2fc1738fef8632b52373581d8ce1609a943679
aparnakr <aparnalocked@gmail.com>
2021-01-17 11:36:03 -0800

    Revert "Feature/0xtrade with permit (#351)"
    This reverts commit 861cf1b89998aa0ddba20d6cd15db6ee67008083.

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-$(npm bin)/ganache-cli --port 8545 --fork https://mainnet.infura.io/v3/$INFURA_KEY@11655503 --unlock 0xbe0eb53f46cd790cd13851d5eff43d12404d33e8 --unlock 0x638E5DA0EEbbA58c67567bcEb4Ab2dc8D34853FB > /dev/null 2>&1 & sleep 10 && node --max-old-space-size=4096 $(npm bin)/truffle test ./test/mainnet-fork/*.ts
+$(npm bin)/ganache-cli --fork https://mainnet.infura.io/v3/$INFURA_KEY --unlock 0xbe0eb53f46cd790cd13851d5eff43d12404d33e8 > /dev/null 2>&1 & sleep 10 && node --max-old-space-size=4096 $(npm bin)/truffle test ./test/e2e/*.ts
