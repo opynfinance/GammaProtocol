671d5f204e3f18b92125182443ffb45c4580d3df
Anton Cheng <antonassocareer@gmail.com>
2021-03-12 14:59:40 +0800

    lint

---
+++ migrations/3_setup_ownership.js
@@ -0,0 +1,29 @@
+// import contract
+const Whitelist = artifacts.require('Whitelist')
+const Oracle = artifacts.require('Oracle')
+const MarginPool = artifacts.require('MarginPool')
+const AddressBook = artifacts.require('AddressBook')
+const Controller = artifacts.require('Controller')
+
+// import config file
+const deploymentConfig = require('./deployment-config.json')
+
+module.exports = async function(deployer, network, accounts) {
+  const [deployerAddress] = accounts
+
+  // new protocol owner
+  const newOwner = deploymentConfig.MULTISIG
+
+  const addressbook = await AddressBook.deployed()
+  const whitelist = await Whitelist.deployed()
+  const oracle = await Oracle.deployed()
+  const pool = await MarginPool.deployed()
+  const controller = await Controller.at(await addressbook.getController())
+
+  // transfer ownership
+  await addressbook.transferOwnership(newOwner, { from: deployerAddress })
+  await whitelist.transferOwnership(newOwner, { from: deployerAddress })
+  await oracle.transferOwnership(newOwner, { from: deployerAddress })
+  await pool.transferOwnership(newOwner, { from: deployerAddress })
+  await controller.transferOwnership(newOwner, { from: deployerAddress })
+}
