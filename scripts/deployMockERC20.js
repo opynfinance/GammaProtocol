7ccaef08e5d27f4eb2e24779f9410e1850de6212
Anton Cheng <antonassocareer@gmail.com>
2021-03-22 15:27:54 +0800

    Update script to deploy MockERC20 and MockPermitERC20

--- scripts/deployMockERC20.js
+++ scripts/deployMockERC20.js
@@ -1,21 +1,23 @@
 const yargs = require('yargs')
 
 const ERC20 = artifacts.require('MockERC20.sol')
+const PermitERC20 = artifacts.require('MockPermitERC20.sol')
 
 module.exports = async function(callback) {
   try {
     const options = yargs
-      .usage('Usage: --name <name> --symbol <symbol> --decimals <decimals> --gas <gasPrice>')
+      .usage('Usage: --name <name> --symbol <symbol> --decimals <decimals> --permit <permit> --gas <gasPrice>')
       .option('name', {describe: 'Token Name', type: 'string', demandOption: true})
       .option('symbol', {describe: 'Token Symbol', type: 'string', demandOption: true})
       .option('decimals', {describe: 'Token Decimals', type: 'string', demandOption: true})
+      .option('permit', {describe: 'Support Permit or not', type: 'boolean', decimals: true})
       .option('gas', {describe: 'Gas price in WEI', type: 'string', demandOption: false}).argv
 
     console.log(`Deploying MockERC20: ${options.symbol} contract on ${options.network} 🍕`)
 
-    const tx = await ERC20.new(options.name, options.symbol, options.decimals, {
-      gasPrice: options.gas,
-    })
+    const tx = options.permit
+      ? await PermitERC20.new(options.name, options.symbol, options.decimals, {gasPrice: options.gas})
+      : await ERC20.new(options.name, options.symbol, options.decimals, {gasPrice: options.gas})
 
     console.log('MockERC20 deployed! 🎉')
     console.log(`Transaction hash: ${tx.transactionHash}`)
