e5b1f50d3d33080f11d08948a7403581641650b2
Anton Cheng <antonassocareer@gmail.com>
2021-04-19 08:57:57 +0800

    lint

--- scripts/deployOtokenImpl.js
+++ scripts/deployOtokenImpl.js
@@ -1,26 +1,24 @@
-const yargs = require("yargs");
+const yargs = require('yargs')
 
-const Otoken = artifacts.require("Otoken.sol");
+const Otoken = artifacts.require('Otoken.sol')
 
 module.exports = async function(callback) {
-    try {
-        const options = yargs
-            .usage("Usage: --network <network> --gas <gasPrice>")
-            .option("network", { describe: "Network name", type: "string", demandOption: true })
-            .option("gas", { describe: "Gas price in WEI", type: "string", demandOption: false })
-            .argv;
+  try {
+    const options = yargs
+      .usage('Usage: --network <network> --gas <gasPrice>')
+      .option('network', {describe: 'Network name', type: 'string', demandOption: true})
+      .option('gas', {describe: 'Gas price in WEI', type: 'string', demandOption: false}).argv
 
-        console.log(`Deploying chainlink pricer contract on ${options.network} 🍕`)
+    console.log(`Deploying chainlink pricer contract on ${options.network} 🍕`)
 
-        const tx = await Otoken.new({gasPrice: options.gas});
+    const tx = await Otoken.new({gasPrice: options.gas})
 
-        console.log("Otoken implementation deployed! 🎉");
-        console.log(`Transaction hash: ${tx.transactionHash}`);
-        console.log(`Deployed contract address: ${tx.address}`);
+    console.log('Otoken implementation deployed! 🎉')
+    console.log(`Transaction hash: ${tx.transactionHash}`)
+    console.log(`Deployed contract address: ${tx.address}`)
 
-        callback();
-    }
-    catch(err) {
-        callback(err);
-    }
-} 
+    callback()
+  } catch (err) {
+    callback(err)
+  }
+}
