8bb8ea99618ca882bf20c914c51672bddbf0319f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-06-07 10:39:32 -0700

    build: port truffle migrations to hardhat deployment script, update npm deployment scripts

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require('Migrations')
-
-module.exports = function(deployer, network, accounts) {
-  const account = accounts[0]
-  console.log(`deploying with account ${account}`)
-  deployer.deploy(Migrations)
-}
