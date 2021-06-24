df9c903b6bc4197aaf4fd27249aeeaf8ffcf96fa
Anton Cheng <antonassocareer@gmail.com>
2021-06-24 08:28:30 +0800

    subgraph migration oracle

--- scripts/migrateOracle.js
+++ scripts/migrateOracle.js
@@ -6,17 +6,73 @@ const OtokenFactory = artifacts.require('OtokenFactory.sol')
 const Oracle = artifacts.require('Oracle.sol')
 const Otoken = artifacts.require('Otoken.sol')
 
+const fetch = require('node-fetch')
+
+const postQuery = async (endpoint, query) => {
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({query}),
+  }
+  const url = endpoint
+  const response = await fetch(url, options)
+  const data = await response.json()
+  if (data.errors) {
+    throw new Error(data.errors[0].message)
+  } else {
+    return data
+  }
+}
+
+/**
+ * Get all oTokens
+ */
+async function getOTokens() {
+  const currentTimeStamp = Math.floor(Date.now() / 1000)
+
+  const query = `
+  {
+    otokens (
+      first: 500,
+      where: {
+        expiryTimestamp_lt: ${currentTimeStamp},
+      }
+    )  {
+      strikeAsset {
+        id
+      }
+      underlyingAsset {
+        id
+      }
+      collateralAsset {
+        id
+      }
+      isPut
+      expiryTimestamp
+    }
+  }`
+  try {
+    const response = await postQuery('https://api.thegraph.com/subgraphs/name/opynfinance/gamma-kovan', query)
+    return response.data.otokens
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
 module.exports = async function(callback) {
   try {
     const options = yargs
-      .usage('Usage: --network <network> --factory <otokenFactory> --oldOracle <oldOracle> --newOracle <newOracle> --asset <asset> --gasPrice <gasPrice>')
+      .usage(
+        'Usage: --network <network> --factory <otokenFactory> --oldOracle <oldOracle> --newOracle <newOracle> --asset <asset> --gasPrice <gasPrice>',
+      )
       .option('network', {describe: '0x exchange address', type: 'string', demandOption: true})
       .option('factory', {describe: 'Otoken factory address', type: 'string', demandOption: true})
       .option('oldOracle', {describe: 'Old oracle module address', type: 'string', demandOption: true})
       .option('newOracle', {describe: 'New oracle module address', type: 'string', demandOption: true})
       .option('asset', {describe: 'Asset address to migrate', type: 'string', demandOption: true})
       .option('gasPrice', {describe: 'Gas price in WEI', type: 'string', demandOption: false}).argv
-  
+
     console.log('Init contracts 🍕')
 
     const factory = await OtokenFactory.at(options.factory)
@@ -25,22 +81,41 @@ module.exports = async function(callback) {
 
     console.log(`Getting list of create Otokens with asset ${options.asset} 🍕`)
 
-    const currentTimeStamp = Math.floor(Date.now() / 1000);
+    const currentTimeStamp = Math.floor(Date.now() / 1000)
     const otokenLength = new BigNumber(await factory.getOtokensLength())
-    
-    let expiriesToMigrate = []
-    let pricesToMigrate = []
 
-    for(let i = 0; i < otokenLength.toFixed(); i++) {
-      const otokenAddress = await factory.otokens(i)
-      const otokenDetails = await (await Otoken.at(otokenAddress)).getOtokenDetails()
-      const expiry = new BigNumber(otokenDetails[4])
+    const expiriesToMigrate = []
+    const pricesToMigrate = []
 
-      if((otokenDetails[0] == options.asset || otokenDetails[1] == options.asset || otokenDetails[2] == options.asset) && (expiry.toNumber() < currentTimeStamp) && (!expiriesToMigrate.includes(expiry.toString()))) {
+    console.log(`total tokens: ${otokenLength}`)
+
+    const otokens = await getOTokens()
+
+    console.log(`fromSubgraph: ${otokens.length}`)
+
+    for (let i = 0; i < otokens.length; i++) {
+      // const otokenAddress = await factory.otokens(i)
+
+      const otoken = otokens[i]
+
+      // const otoken = await Otoken.at(otokenAddress)
+
+      const strike = otoken.strikeAsset.id
+      const collateral = otoken.collateralAsset.id
+      const underlying = otoken.underlyingAsset.id
+      const expiry = otoken.expiryTimestamp
+      if (
+        (strike.toLowerCase() == options.asset.toLowerCase() ||
+          collateral.toLowerCase() == options.asset.toLowerCase() ||
+          underlying.toLowerCase() == options.asset.toLowerCase()) &&
+        !expiriesToMigrate.includes(expiry.toString())
+      ) {
         const priceToMigrate = new BigNumber((await oldOracle.getExpiryPrice(options.asset, expiry.toString()))[0])
 
         expiriesToMigrate.push(expiry.toString())
         pricesToMigrate.push(priceToMigrate.toString())
+        console.log('pushed', expiriesToMigrate.length)
+      } else {
       }
     }
 
@@ -50,7 +125,7 @@ module.exports = async function(callback) {
     const tx = await newOracle.migrateOracle(options.asset, expiriesToMigrate, pricesToMigrate)
 
     console.log(`Oracle prices migration done for asset ${options.asset}! 🎉`)
-    console.log(`Transaction hash: ${tx.transactionHash}`)
+    console.log(`Transaction hash: ${tx.tx}`)
 
     callback()
   } catch (err) {
