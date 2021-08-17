89ce3b70947f39f8918c49c0cb6ddc1726a0bbde
Haythem Sellami <sellamihaythem96@gmail.com>
2021-08-17 16:07:51 +0100

    change mock from STETH to WSTETH

--- contracts/mocks/MockWSTETHToken.sol
+++ contracts/mocks/MockWSTETHToken.sol
@@ -3,8 +3,8 @@ pragma solidity 0.6.10;
 
 import {ERC20Upgradeable} from "../packages/oz/upgradeability/ERC20Upgradeable.sol";
 
-contract MockSTETHToken is ERC20Upgradeable {
-    mapping(uint256 => uint256) public getPooledEthByShares;
+contract MockWSTETHToken is ERC20Upgradeable {
+    uint256 public stEthPerToken;
 
     constructor(string memory _name, string memory _symbol) public {
         __ERC20_init_unchained(_name, _symbol);
@@ -15,7 +15,7 @@ contract MockSTETHToken is ERC20Upgradeable {
         _mint(account, amount);
     }
 
-    function setPooledEthByShares(uint256 _pooledEthByShares) external {
-        getPooledEthByShares[1 ether] = _pooledEthByShares;
+    function setStEthPerToken(uint256 _stEthPerToken) external {
+        stEthPerToken = _stEthPerToken;
     }
 }
